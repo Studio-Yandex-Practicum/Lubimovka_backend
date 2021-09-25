6c70e0b30c9a8c4e65faf8b140f0258b8a0308cf
Iki-oops <bambagaevdmitrij@gmail.com>
2021-09-25 22:43:01 +0800

    add presentpiece model

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.core.validators import RegexValidator
 
 from apps.core.models import BaseModel
 
@@ -37,3 +40,25 @@ class Project(BaseModel):
     class Meta:
         verbose_name = "Проект"
         verbose_name_plural = "Проекты"
+
+
+class PresentPiece(BaseModel):
+    first_name = models.CharField(max_length=200, verbose_name="Имя")
+    last_name = models.CharField(max_length=200, verbose_name="Фамилия")
+    birthday = models.DateField(verbose_name="День рождения")
+    city = models.CharField(max_length=50, verbose_name="Город проживания")
+    phone_regex = r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
+    phone_number = models.CharField(
+        max_length=12,
+        validators=[RegexValidator(regex=phone_regex)],
+        verbose_name="Номер телефона",
+    )
+    email = models.EmailField(max_length=100, verbose_name="Электронная почта")
+    title = models.CharField(max_length=200, verbose_name="Название пьесы")
+    year = models.CharField(
+        max_length=4,
+        verbose_name="Год написания",
+        default=datetime.date.today().year,
+    )
+    file_link = models.URLField(verbose_name="Ссылка на файл")
+    status = models.BooleanField(verbose_name="Статус")
