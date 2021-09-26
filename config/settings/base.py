99151d67ab123656a18a2c58713f0608c3a312ac
DRAGANmik <75855531+DRAGANmik@users.noreply.github.com>
2021-09-27 01:34:27 +0300

    corrections after review

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,9 +115,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAdminUser",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
