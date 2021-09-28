9a9242634146a564696ca7d0bebc74148312e83d
Nikolay Kiryanov <nikolay@kiryanov.ru>
2021-09-29 01:52:39 +0300

    WIP some basic models

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from apps.articles.models import BlogItem, NewsItem, Project
+from apps.articles.models import (
+    BlockText,
+    BlockTitle,
+    BlogItem,
+    Content,
+    NewsItem,
+    Project,
+)
 
 
 class NewsItemAdmin(admin.ModelAdmin):
@@ -13,6 +20,21 @@ class BlogItemAdmin(admin.ModelAdmin):
 
 class ProjectAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(BlockText)
+class BlockTextAdmin(admin.ModelAdmin):
+    list_display = ["text"]
+
+
+@admin.register(BlockTitle)
+class BlockTitleAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ["name"]
 
 
 admin.site.register(NewsItem, NewsItemAdmin)
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from apps.core.models import BaseModel
@@ -37,3 +39,53 @@ class Project(BaseModel):
     class Meta:
         verbose_name = "Проект"
         verbose_name_plural = "Проекты"
+
+
+class BlockTitle(BaseModel):
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Блок заголовок",
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class BlockText(BaseModel):
+    text = models.TextField(
+        max_length=500,
+        verbose_name="Блок с текстом",
+    )
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Content(BaseModel):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+    project = models.ForeignKey(
+        Project,
+        related_name="modules",
+        on_delete=models.CASCADE,
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            "model__in": (
+                "blocktitle",
+                "blocktext",
+            )
+        },
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return self.name
