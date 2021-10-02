7d82e64114498a9b0d0a58da429980bedd534d5a
Iki-oops <bambagaevdmitrij@gmail.com>
2021-10-02 19:16:05 +0800

    moved model to app library

--- apps/library/models.py
+++ apps/library/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from apps.core.models import BaseModel
 
@@ -60,3 +63,27 @@ class PerformanceReview(BaseModel):
     class Meta:
         verbose_name = "Отзыв зрителя на спектакль"
         verbose_name_plural = "Отзывы зрителей на спектакль"
+
+
+class ParticipationApplicationFestival(BaseModel):
+    first_name = models.CharField(max_length=200, verbose_name="Имя")
+    last_name = models.CharField(max_length=200, verbose_name="Фамилия")
+    birthday = models.DateField(verbose_name="День рождения")
+    city = models.CharField(max_length=50, verbose_name="Город проживания")
+    phone_number = PhoneNumberField()
+    email = models.EmailField(max_length=100, verbose_name="Электронная почта")
+    title = models.CharField(max_length=200, verbose_name="Название пьесы")
+    year = models.CharField(
+        max_length=4,
+        verbose_name="Год написания",
+        default=datetime.date.today().year,
+    )
+    file_link = models.URLField(verbose_name="Ссылка на файл")
+    status = models.BooleanField(verbose_name="Статус")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Заявления на участие'
+        verbose_name = 'Заявление на участие'
