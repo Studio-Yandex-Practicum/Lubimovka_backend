bd38589b3e67aaf96e48c575f3ff29c895f02d98
Дубинчик Андрей <evil_ghost@bk.ru>
2021-10-03 02:19:57 +0300

    Event model implemented

--- apps/afisha/admin.py
+++ apps/afisha/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
-from apps.afisha.models import Event
+from apps.afisha.models import BaseEvent, Event
+
+
+class BaseEventAdmin(admin.ModelAdmin):
+    list_display = ("pk",)
+    list_filter = ("created",)
+    empty_value_display = "-пусто-"
 
 
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("pk", "base_event", "type", "date_time", "paid")
+    list_filter = ("type",)
+    empty_value_display = "-пусто-"
 
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(BaseEvent, BaseEventAdmin)
--- apps/afisha/models.py
+++ apps/afisha/models.py
@@ -3,13 +3,59 @@ from django.db import models
 from apps.core.models import BaseModel
 
 
-class Event(BaseModel):
-    name = models.CharField(
-        max_length=200,
-        unique=True,
-        verbose_name="Название события",
-    )
+class BaseEvent(BaseModel):
+    def __str__(self):
+        if hasattr(self, "masterclasses"):
+            return self.masterclasses.name
+        elif hasattr(self, "readings"):
+            return self.readings.name
+        elif hasattr(self, "performances"):
+            return self.performances.name
+        else:
+            return str(self.pk)
 
     class Meta:
+        ordering = ("-created",)
+        verbose_name = "Базовое событие"
+        verbose_name_plural = "Базовые события"
+
+
+class Event(BaseModel):
+    PERFORMANCE = "performance"
+    MASTER_CLASS = "masterclass"
+    READING = "reading"
+    EVENT_TYPES = [
+        (PERFORMANCE, "Спектакль"),
+        (MASTER_CLASS, "Мастер-класс"),
+        (READING, "Читка"),
+    ]
+
+    base_event = models.ForeignKey(
+        BaseEvent,
+        on_delete=models.CASCADE,
+        related_name="body",
+        verbose_name="Событие",
+    )
+    type = models.CharField(
+        verbose_name="Тип события",
+        choices=EVENT_TYPES,
+        default=PERFORMANCE,
+        max_length=50,
+    )
+    date_time = models.DateTimeField(verbose_name="Дата и время")
+    paid = models.BooleanField(verbose_name="Платное")
+    url = models.URLField(
+        max_length=200,
+        blank=True,
+        verbose_name="Ссылка",
+        unique=True,
+    )
+    place = models.CharField(verbose_name="Место", max_length=200)
+
+    def __str__(self):
+        return f"{self.base_event} - {self.type}, {self.date_time}"
+
+    class Meta:
+        ordering = ("-created",)
         verbose_name = "Событие"
         verbose_name_plural = "События"
