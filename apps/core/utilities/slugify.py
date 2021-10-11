9ad90581967a8da370a3414604cd5bfcafaec4ef
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-10-12 02:02:04 +0300

    add person factory and slugify utilite

---
+++ apps/core/utilities/slugify.py
@@ -0,0 +1,41 @@
+from django.template.defaultfilters import slugify as django_slugify
+
+ALPHABET = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ё": "yo",
+    "ж": "zh",
+    "з": "z",
+    "и": "i",
+    "й": "j",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "х": "kh",
+    "ц": "ts",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "shch",
+    "ы": "i",
+    "э": "e",
+    "ю": "yu",
+    "я": "ya",
+}
+
+
+def slugify(name):
+    return django_slugify(
+        "".join(ALPHABET.get(char, char) for char in name.lower())
+    )
