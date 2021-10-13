45e41f6107fe9de6f758bb542551fb514e432a84
Анна Полякова <Sova040888@yandex.ru>
2021-10-13 19:37:00 +0300

    fix

--- apps/info/views/question.py
+++ apps/info/views/question.py
@@ -1,15 +1,8 @@
-from rest_framework import serializers
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import generics
 
-from apps.info.models import Question
+from apps.info.serializers.question import QuestionSerializer
 from apps.info.utils import send_question
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = "__all__"
 
 
 class QuestionCreateAPI(generics.CreateAPIView):
