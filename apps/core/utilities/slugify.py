5ba890191eff49bf76484454fe84a67543544a2c
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-10-17 22:56:57 +0300

    fix after review

--- apps/core/utilities/slugify.py
+++ apps/core/utilities/slugify.py
@@ -1,38 +1,6 @@
 from django.template.defaultfilters import slugify as django_slugify
 
-ALPHABET = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "yo",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "й": "j",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "ts",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "shch",
-    "ы": "i",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
-}
+from apps.core.utilities.constants import ALPHABET
 
 
 def slugify(name):
--- apps/info/urls.py
+++ apps/info/urls.py
@@ -11,23 +11,53 @@ from apps.info.views import (
 )
 
 festival_urls = [
-    path("what-we-do/", WhatWeDoViewSet.as_view(), name="what-we-do"),
+    path(
+        "what-we-do/",
+        WhatWeDoViewSet.as_view(),
+        name="what-we-do",
+    ),
     path(
         "festival-teams/",
         FestivalTeamsViewSet.as_view(),
         name="festival-teams",
     ),
-    path("sponsors/", SponsorViewSet.as_view(), name="sponsors"),
-    path("volunteers/", VolunteersViewSet.as_view(), name="volunteers"),
-    path("ideology/", IdeologyViewSet.as_view(), name="ideology"),
+    path(
+        "sponsors/",
+        SponsorViewSet.as_view(),
+        name="sponsors",
+    ),
+    path(
+        "volunteers/",
+        VolunteersViewSet.as_view(),
+        name="volunteers",
+    ),
+    path(
+        "ideology/",
+        IdeologyViewSet.as_view(),
+        name="ideology",
+    ),
 ]
 
 info_urls = [
-    path("about-festival/", include(festival_urls)),
-    path("partners/", PartnersViewSet.as_view(), name="partners"),
-    path("questions/", QuestionCreateAPI.as_view(), name="questions"),
+    path(
+        "about-festival/",
+        include(festival_urls),
+    ),
+    path(
+        "partners/",
+        PartnersViewSet.as_view(),
+        name="partners",
+    ),
+    path(
+        "questions/",
+        QuestionCreateAPI.as_view(),
+        name="questions",
+    ),
 ]
 
 urlpatterns = [
-    path("v1/", include(info_urls)),
+    path(
+        "v1/",
+        include(info_urls),
+    ),
 ]
