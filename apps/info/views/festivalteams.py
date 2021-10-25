becf917e3cad9d56dede1f4d0d4266a22f28988a
DRAGANmik <75855531+DRAGANmik@users.noreply.github.com>
2021-10-25 16:20:29 +0300

    set up default permissions as AllowAny

--- apps/info/views/festivalteams.py
+++ apps/info/views/festivalteams.py
@@ -1,6 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
 
 from apps.info.models import FestivalTeam
 from apps.info.serializers import FestivalTeamsSerializer
@@ -9,7 +8,6 @@ from apps.info.serializers import FestivalTeamsSerializer
 class FestivalTeamsViewSet(ListAPIView):
     queryset = FestivalTeam.objects.all()
     serializer_class = FestivalTeamsSerializer
-    permission_classes = (AllowAny,)
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("team",)
     pagination_class = None
--- apps/info/views/sponsors.py
+++ apps/info/views/sponsors.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
 
 from apps.info.models import Sponsor
 from apps.info.serializers import SponsorSerializer
@@ -8,5 +7,4 @@ from apps.info.serializers import SponsorSerializer
 class SponsorViewSet(ListAPIView):
     queryset = Sponsor.objects.all()
     serializer_class = SponsorSerializer
-    permission_classes = (AllowAny,)
     pagination_class = None
