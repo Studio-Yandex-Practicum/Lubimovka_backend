246d2483e71794122cb5b4438e6cbefd5ad2e97a
Konstantin Raikhert <raikhert13@gmail.com>
2021-10-26 22:44:13 +0300

    fix after review abd add codestyle about list and turple

--- apps/info/views/volunteers.py
+++ apps/info/views/volunteers.py
@@ -1,4 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
 from apps.info.models import Volunteer
@@ -8,6 +7,5 @@ from apps.info.serializers import VolunteersSerializer
 class VolunteersViewSet(ListAPIView):
     queryset = Volunteer.objects.all()
     serializer_class = VolunteersSerializer
-    filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("year",)
     pagination_class = None
