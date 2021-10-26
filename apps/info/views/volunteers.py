5fb731ff6e7957e3213498c643d73d3771d4ced3
Konstantin Raikhert <raikhert13@gmail.com>
2021-10-26 21:51:47 +0300

    add query year for volunteers

--- apps/info/views/volunteers.py
+++ apps/info/views/volunteers.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
 from apps.info.models import Volunteer
@@ -7,4 +8,6 @@ from apps.info.serializers import VolunteersSerializer
 class VolunteersViewSet(ListAPIView):
     queryset = Volunteer.objects.all()
     serializer_class = VolunteersSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("year",)
     pagination_class = None
