d1338ebb2305e6b8b20e1c4189334b5a877476cd
19160393 <PGVasilyeva@sberbank.ru>
2021-11-17 20:34:22 +0300

    add a ckeditor

--- config/settings/local.py
+++ config/settings/local.py
@@ -24,6 +24,8 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+# for CKEditor
+INSTALLED_APPS += ["ckeditor"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
