4fca9970c5c9cdf4c71a5df580fd962c3ff7f756
Dragan Mihail <75855531+DRAGANmik@users.noreply.github.com>
2021-11-27 18:20:16 +0300

    refactoring

--- apps/articles/filters.py
+++ apps/articles/filters.py
@@ -8,9 +8,3 @@ class PubDateFilter(django_filters.FilterSet):
     year = django_filters.NumberFilter(
         field_name="pub_date", lookup_expr="year"
     )
-    o = django_filters.OrderingFilter(
-        choices=(("pub_date", "Month"),),
-        fields={
-            "month": "pub_date",
-        },
-    )
