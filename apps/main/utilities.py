04012540a87b2a99a581aec8214962e9b12afbb2
Nikolay Kiryanov <nikolay@kiryanov.ru>
2021-12-03 02:09:21 +0300

    Refactoring: Main Reppresentation Update

--- apps/main/utilities.py
+++ apps/main/utilities.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import date, timedelta
 
 from apps.afisha.models import Event
 from apps.articles.models import BlogItem, NewsItem
 from apps.core.models import Settings
 from apps.info.models import Festival, Place
-from apps.library.models import ProgramType
+from apps.library.models import Play, ProgramType
 from apps.main.models import Banner
 
 
@@ -12,27 +12,31 @@ class MainObject:
     def add_first_screen_data(self):
         main_add_first_screen = Settings.get_setting("main_add_first_screen")
         if main_add_first_screen:
-            self.first_screen_title = Settings.get_setting(
-                "main_first_screen_title"
-            )
-            self.first_screen_url_title = Settings.get_setting(
-                "main_first_screen_url_title"
-            )
-            self.first_screen_url = Settings.get_setting(
-                "main_first_screen_url"
-            )
+            title = Settings.get_setting("main_first_screen_title")
+            url_title = Settings.get_setting("main_first_screen_url_title")
+            url = Settings.get_setting("main_first_screen_url")
+            self.first_screen = dict()
+            self.first_screen["title"] = title
+            self.first_screen["url_title"] = url_title
+            self.first_screen["url"] = url
 
     def add_blog_data(self):
         main_add_blog = Settings.get_setting("main_add_blog")
         if main_add_blog:
-            self.blog_items = BlogItem.ext_objects.published()[:6]
-            self.blog_title = Settings.get_setting("main_blog_title")
+            title = Settings.get_setting("main_blog_title")
+            items = BlogItem.ext_objects.published()[:6]
+            self.blog = dict()
+            self.blog["title"] = title
+            self.blog["items"] = items
 
     def add_news_data(self):
         main_add_news = Settings.get_setting("main_add_news")
         if main_add_news:
-            self.news_items = NewsItem.ext_objects.published()[:6]
-            self.news_title = Settings.get_setting("main_news_title")
+            title = Settings.get_setting("main_news_title")
+            items = NewsItem.ext_objects.published()[:6]
+            self.news = dict()
+            self.news["title"] = title
+            self.news["items"] = items
 
     def add_afisha(self):
         main_add_afisha = Settings.get_setting("main_add_afisha")
@@ -41,46 +45,54 @@ class MainObject:
                 "main_show_afisha_only_for_today"
             )
             if main_show_afisha_only_for_today:
-                today = datetime.datetime.now().date()
-                tomorrow = today + datetime.timedelta(days=1)
-                event_items = Event.objects.filter(
+                today = date.today()
+                tomorrow = today + timedelta(days=1)
+                items = Event.objects.filter(
                     date_time__range=(today, tomorrow),
                     pinned_on_main=True,
                 )
             else:
-                event_items = Event.objects.filter(pinned_on_main=True)[:6]
-            self.event_items = event_items
-            self.event_title = Settings.get_setting("main_afisha_title")
+                items = Event.objects.filter(pinned_on_main=True)[:6]
+            title = Settings.get_setting("main_afisha_title")
+            self.afisha = dict()
+            self.afisha["title"] = title
+            self.afisha["items"] = items
 
     def add_banners(self):
         main_add_banners = Settings.get_setting("main_add_banners")
         if main_add_banners:
-            self.banner_items = Banner.objects.all()
+            items = Banner.objects.all()
+            self.banners = dict()
+            self.banners["items"] = items
 
     def add_short_list(self):
         main_add_short_list = Settings.get_setting("main_add_short_list")
         if main_add_short_list:
             program = ProgramType.objects.get(slug="short-list")
             festival = Festival.objects.all().order_by("-year").first()
-            shot_list_plays = program.plays.filter(
-                festival=festival, is_draft=False
-            )[:4]
-            self.short_list_items = shot_list_plays
-            self.short_list_title = Settings.get_setting(
-                "main_short_list_title"
+            short_list_plays = Play.objects.filter(
+                program=program,
+                festival=festival,
+                is_draft=False,
             )
+            title = Settings.get_setting("main_short_list_title")
+            items = short_list_plays[:4]
+            self.short_list = dict()
+            self.short_list["title"] = title
+            self.short_list["items"] = items
 
     def add_video_archive(self):
         main_add_video_archive = Settings.get_setting("main_add_video_archive")
         if main_add_video_archive:
-            self.video_archive_url = Settings.get_setting(
-                "main_video_archive_url"
-            )
-            self.video_archive_photo = Settings.get_setting(
-                "main_video_archive_photo"
-            )
+            url = Settings.get_setting("main_video_archive_url")
+            photo = Settings.get_setting("main_video_archive_photo")
+            self.video_archive = dict()
+            self.video_archive["url"] = url
+            self.video_archive["photo"] = photo
 
     def add_places(self):
         main_add_places = Settings.get_setting("main_add_places")
         if main_add_places:
-            self.place_items = Place.objects.all()
+            items = Place.objects.all()
+            self.places = dict()
+            self.places["items"] = items
