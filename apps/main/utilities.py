2abcd130676102eb3303281bb61c2cc95637d355
Nikolay Kiryanov <nikolay@kiryanov.ru>
2021-12-03 02:45:51 +0300

    update: refactoring after code review

--- apps/main/utilities.py
+++ apps/main/utilities.py
@@ -15,28 +15,31 @@ class MainObject:
             title = Settings.get_setting("main_first_screen_title")
             url_title = Settings.get_setting("main_first_screen_url_title")
             url = Settings.get_setting("main_first_screen_url")
-            self.first_screen = dict()
-            self.first_screen["title"] = title
-            self.first_screen["url_title"] = url_title
-            self.first_screen["url"] = url
+            self.first_screen = {
+                "title": title,
+                "url_title": url_title,
+                "url": url,
+            }
 
     def add_blog_data(self):
         main_add_blog = Settings.get_setting("main_add_blog")
         if main_add_blog:
             title = Settings.get_setting("main_blog_title")
             items = BlogItem.ext_objects.published()[:6]
-            self.blog = dict()
-            self.blog["title"] = title
-            self.blog["items"] = items
+            self.blog = {
+                "title": title,
+                "items": items,
+            }
 
     def add_news_data(self):
         main_add_news = Settings.get_setting("main_add_news")
         if main_add_news:
             title = Settings.get_setting("main_news_title")
             items = NewsItem.ext_objects.published()[:6]
-            self.news = dict()
-            self.news["title"] = title
-            self.news["items"] = items
+            self.news = {
+                "title": title,
+                "items": items,
+            }
 
     def add_afisha(self):
         main_add_afisha = Settings.get_setting("main_add_afisha")
@@ -54,45 +57,45 @@ class MainObject:
             else:
                 items = Event.objects.filter(pinned_on_main=True)[:6]
             title = Settings.get_setting("main_afisha_title")
-            self.afisha = dict()
-            self.afisha["title"] = title
-            self.afisha["items"] = items
+            self.afisha = {
+                "title": title,
+                "items": items,
+            }
 
     def add_banners(self):
         main_add_banners = Settings.get_setting("main_add_banners")
         if main_add_banners:
             items = Banner.objects.all()
-            self.banners = dict()
-            self.banners["items"] = items
+            self.banners = {"items": items}
 
     def add_short_list(self):
         main_add_short_list = Settings.get_setting("main_add_short_list")
         if main_add_short_list:
             program = ProgramType.objects.get(slug="short-list")
             festival = Festival.objects.all().order_by("-year").first()
-            short_list_plays = Play.objects.filter(
+            items = Play.objects.filter(
                 program=program,
                 festival=festival,
                 is_draft=False,
-            )
+            )[:4]
             title = Settings.get_setting("main_short_list_title")
-            items = short_list_plays[:4]
-            self.short_list = dict()
-            self.short_list["title"] = title
-            self.short_list["items"] = items
+            self.short_list = {
+                "title": title,
+                "items": items,
+            }
 
     def add_video_archive(self):
         main_add_video_archive = Settings.get_setting("main_add_video_archive")
         if main_add_video_archive:
             url = Settings.get_setting("main_video_archive_url")
             photo = Settings.get_setting("main_video_archive_photo")
-            self.video_archive = dict()
-            self.video_archive["url"] = url
-            self.video_archive["photo"] = photo
+            self.video_archive = {
+                "url": url,
+                "photo": photo,
+            }
 
     def add_places(self):
         main_add_places = Settings.get_setting("main_add_places")
         if main_add_places:
             items = Place.objects.all()
-            self.places = dict()
-            self.places["items"] = items
+            self.places = {"items": items}
