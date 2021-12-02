0f4d248e528b4eb9fa6b526b7912c240ebb0bb1d
MarselMulyukov <MarekMulin@yandex.ru>
2021-12-03 04:11:12 +0500

    library_factories

--- apps/core/management/commands/filldb.py
+++ apps/core/management/commands/filldb.py
@@ -10,11 +10,19 @@ from apps.info.tests.factories import (
     SponsorFactory,
     VolunteerFactory,
 )
+from apps.library.tests.factories import (
+    AuthorFactory,
+    MasterClassFactory,
+    ParticipationApplicationFestivalFactory,
+    PerformanceFactory,
+    ProgramFactory,
+    ReadingFactory,
+)
 
 
 def notification(command, objects, text):
     command.stdout.write(
-        command.style.SUCCESS(f"{len(objects)} {text} успешно созданы.")
+        command.style.SUCCESS(f"{len(objects)} {text} успешно создано.")
     )
 
 
@@ -30,6 +38,19 @@ class Command(BaseCommand):
         " - Команды фестиваля"
         " - Пользователи-админы"
         " - Пользователи-редакторы"
+        " - Программы"
+        " - Авторы"
+        " - Ссылки на сторонние ресурсы"
+        " - Пьесы"
+        " - Достижения"
+        " - Ссылки на социальные сети"
+        " - Другие пьесы"
+        " - Спектакли"
+        " - Отзывы на спектакли"
+        " - Медиа отзывы на спектакли"
+        " - Мастер-классы"
+        " - Читки"
+        " - Члены команды читок, спектаклей и мастер-классов"
     )
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
@@ -71,8 +92,13 @@ class Command(BaseCommand):
             festivals = FestivalFactory.create_batch(10)
             notification(self, festivals, "фестивалей")
 
+            programtypes = ProgramFactory.create_batch(3)
+            notification(self, programtypes, "программ")
+
             users_editors = []
             users_admins = []
+            authors = []
+            perfomances = []
             for index in range(1, 6):
                 users_editors.append(
                     UserFactory.create(
@@ -84,8 +110,37 @@ class Command(BaseCommand):
                         username=f"admin_{index}", add_role_admin=True
                     )
                 )
+                authors.append(AuthorFactory.complex_create())
+                perfomances.append(PerformanceFactory.complex_create())
+
             notification(self, users_editors, "редакторов")
             notification(self, users_admins, "админов")
+            notification(
+                self,
+                authors,
+                "{}, {}, {}, {}, {}, {}".format(
+                    "авторов",
+                    "пьес",
+                    "достижений",
+                    "ссылок на социальные сети",
+                    "ссылок на сторонние ресурсы",
+                    "других пьес",
+                ),
+            )
+            notification(
+                self, perfomances, "спектаклей, отзывов и медиа отзывов"
+            )
+
+            masterclasses = MasterClassFactory.create_batch(10)
+            notification(self, masterclasses, "мастер-классов")
+
+            readings = ReadingFactory.create_batch(10)
+            notification(self, readings, "читок")
+
+            participations = (
+                ParticipationApplicationFestivalFactory.create_batch(5)
+            )
+            notification(self, participations, "заявок на участие в фестивале")
 
         except CommandError:
             self.stdout.write(self.style.ERROR("Ошибка наполения БД"))
--- apps/library/tests/factories.py
+++ apps/library/tests/factories.py
@@ -1,19 +1,29 @@
-from random import choice
+import random
 
 import factory
+import pytz
 from faker import Faker
 
+from apps.articles.models import Project
 from apps.core.decorators import restrict_factory
-from apps.core.tests.factories import PersonFactory
+from apps.core.models import Person, Role
+from apps.core.tests.factories import ImageFactory, PersonFactory
 from apps.info.models import Festival
 from apps.library.models import (
     Achievement,
     Author,
+    MasterClass,
     OtherLink,
     OtherPlay,
+    ParticipationApplicationFestival,
+    Performance,
+    PerformanceMediaReview,
+    PerformanceReview,
     Play,
     ProgramType,
+    Reading,
     SocialNetworkLink,
+    TeamMember,
 )
 
 fake = Faker("ru_RU")
@@ -38,7 +48,7 @@ class SocialNetworkLinkFactory(factory.django.DjangoModelFactory):
 
     author = factory.Iterator(Author.objects.all())
     name = factory.LazyFunction(
-        lambda: choice(SocialNetworkLink.SocialNetwork.choices)[0]
+        lambda: random.choice(SocialNetworkLink.SocialNetwork.choices)[0]
     )
     link = factory.Faker("url")
 
@@ -78,8 +88,9 @@ class OtherPlayFactory(factory.django.DjangoModelFactory):
 class ProgramFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ProgramType
+        django_get_or_create = ["name"]
 
-    name = factory.LazyFunction(lambda: fake["ru_RU"].word().capitalize())
+    name = factory.LazyFunction(lambda: fake.word().capitalize())
 
 
 @restrict_factory({"global": [Festival, ProgramType]})
@@ -92,12 +103,17 @@ class PlayFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Play
+        django_get_or_create = ["name"]
 
     name = factory.LazyFunction(lambda: fake["ru_RU"].word().capitalize())
     city = factory.Faker("city_name", locale="ru_RU")
     year = factory.Faker("random_int", min=1990, max=2021, step=1)
-    url_download = factory.Faker("url")
-    url_reading = factory.Faker("url")
+    url_download = factory.LazyAttribute(
+        lambda obj: "www.plays-download/{}".format(obj.name)
+    )
+    url_reading = factory.LazyAttribute(
+        lambda obj: "www.plays-reading/{}".format(obj.name)
+    )
     program = factory.Iterator(ProgramType.objects.all())
     festival = factory.Iterator(Festival.objects.all())
 
@@ -180,7 +196,7 @@ class AuthorFactory(factory.django.DjangoModelFactory):
     @factory.post_generation
     def add_play(self, created, extracted, **kwargs):
         """
-        Create a Play object and add it to other_plays_links field for Author.
+        Create a Play object and add it to plays field for Author.
         You should create at least one Festival and Program
         before use this method.
         To use "add_play=True"
@@ -205,3 +221,246 @@ class AuthorFactory(factory.django.DjangoModelFactory):
             add_other_play=True,
             add_play=True,
         )
+
+
+@restrict_factory({"global": [Person, Role]})
+class TeamMemberFactory(factory.django.DjangoModelFactory):
+    """
+    Create TeamMember object.
+    You should create at least one Person and Role
+    before use this factory.
+    This factory is started when you start the Reading,
+    Performance or Master Class factory
+    """
+
+    class Meta:
+        model = TeamMember
+
+    person = factory.Iterator(Person.objects.all())
+    role = factory.Iterator(Role.objects.all())
+
+
+@restrict_factory({"global": [Person, Role]})
+class MasterClassFactory(factory.django.DjangoModelFactory):
+    """
+    Create MasterClass object.
+    You should create at least one Person and Role
+    before use this factory.
+    """
+
+    class Meta:
+        model = MasterClass
+
+    name = factory.LazyFunction(lambda: fake.word().capitalize())
+    description = factory.Faker("text", locale="ru_RU")
+    member = factory.RelatedFactory(
+        TeamMemberFactory,
+        factory_related_name="masterclass",
+        role__name="Ведущий",
+    )
+    project = factory.Iterator(Project.objects.all())
+
+
+@restrict_factory({"global": [Person, Play, Project, Role]})
+class PerformanceFactory(factory.django.DjangoModelFactory):
+    """
+    Create Performance object.
+    You should create at least one Play and Project
+    before use this factory.
+    """
+
+    class Meta:
+        model = Performance
+
+    name = factory.LazyFunction(lambda: fake.word().capitalize())
+    play = factory.Iterator(Play.objects.all())
+    main_image = factory.django.ImageField(
+        color=factory.LazyFunction(
+            lambda: random.choice(["blue", "yellow", "green", "orange"])
+        ),
+        width=factory.LazyFunction(lambda: random.randint(10, 1000)),
+        height=factory.SelfAttribute("width"),
+    )
+    bottom_image = factory.django.ImageField(
+        color=factory.LazyFunction(
+            lambda: random.choice(["blue", "yellow", "green", "orange"])
+        ),
+        width=factory.LazyFunction(lambda: random.randint(10, 1000)),
+        height=factory.SelfAttribute("width"),
+    )
+    description = factory.Faker("text", locale="ru_RU")
+    text = factory.Faker("text", locale="ru_RU")
+    age_limit = factory.LazyFunction(lambda: random.randint(0, 18))
+    dramatist = factory.RelatedFactory(
+        TeamMemberFactory,
+        factory_related_name="performance",
+        role__name="Драматург",
+    )
+    director = factory.RelatedFactory(
+        TeamMemberFactory,
+        factory_related_name="performance",
+        role__name="Режиссёр",
+    )
+    project = factory.Iterator(Project.objects.all())
+
+    @factory.post_generation
+    def add_video(self, created, extracted, **kwargs):
+        """
+        Add video field for Performance
+        To use "add_video=True"
+        """
+        if not created:
+            return
+        if extracted:
+            self.video = factory.Faker("url", locale="ru_RU")
+
+    @factory.post_generation
+    def add_images_in_block(self, created, extracted, **kwargs):
+        """
+        Create random amount of Image objects and add them
+        to images_in_block field for Performance.
+        To use "add_images_in_block=True"
+        """
+        if not created:
+            return
+        if extracted:
+            for _ in range(random.randint(1, 8)):
+                image = ImageFactory.create()
+                self.images_in_block.add(image)
+
+    @factory.post_generation
+    def add_performance_review(self, created, extracted, **kwargs):
+        """
+        Create PerformanceReview object and add
+        to reviews field for Performance.
+        To use "add_performance_review=True"
+        """
+        if not created:
+            return
+        if extracted:
+            PerformanceReviewFactory.create(performance=self)
+
+    @factory.post_generation
+    def add_performance_media_review(self, created, extracted, **kwargs):
+        """
+        Create PerformanceMediaReview object and add
+        to media_reviews field for Performance.
+        To use "add_performance_media_review=True"
+        """
+        if not created:
+            return
+        if extracted:
+            PerformanceMediaReviewFactory.create(performance=self)
+
+    @classmethod
+    def complex_create(cls):
+        """
+        Create Performance object with fully populated fields.
+        You should create at least one Play and Project
+        before use this method.
+        """
+        return cls.create(
+            add_video=True,
+            add_images_in_block=True,
+            add_performance_review=True,
+            add_performance_media_review=True,
+        )
+
+
+@restrict_factory({"global": [Performance]})
+class PerformanceMediaReviewFactory(factory.django.DjangoModelFactory):
+    """
+    Create PerformanceMediaReview object.
+    You should create at least one Performance
+    before use this factory.
+    """
+
+    class Meta:
+        model = PerformanceMediaReview
+
+    media_name = factory.LazyFunction(
+        lambda: fake.sentence(nb_words=random.choice([1, 2, 3])).capitalize()
+    )
+    text = factory.Faker("text", locale="ru_RU")
+    image = factory.django.ImageField(
+        color=factory.LazyFunction(
+            lambda: random.choice(["blue", "yellow", "green", "orange"])
+        ),
+        width=factory.LazyFunction(lambda: random.randint(10, 1000)),
+        height=factory.SelfAttribute("width"),
+    )
+    # performance = factory.Iterator(Performance.objects.all())
+    url = factory.Faker("url")
+    pub_date = factory.LazyFunction(
+        lambda: fake.past_datetime(tzinfo=pytz.UTC)
+    )
+
+
+@restrict_factory({"global": [Performance]})
+class PerformanceReviewFactory(factory.django.DjangoModelFactory):
+    """
+    Create PerformanceReview object.
+    You should create at least one Performance
+    before use this factory.
+    """
+
+    class Meta:
+        model = PerformanceReview
+
+    reviewer_name = factory.Faker("name", locale="ru_RU")
+    text = factory.Faker("text", locale="ru_RU")
+    # performance = factory.Iterator(Performance.objects.all())
+    url = factory.Faker("url")
+    pub_date = factory.LazyFunction(
+        lambda: fake.past_datetime(tzinfo=pytz.UTC)
+    )
+
+
+@restrict_factory({"global": [Person, Play, Project, Role]})
+class ReadingFactory(factory.django.DjangoModelFactory):
+    """
+    Create Reading object.
+    You should create at least one Play and Project
+    before use this factory.
+    """
+
+    class Meta:
+        model = Reading
+
+    play = factory.Iterator(Play.objects.all())
+    name = factory.LazyFunction(lambda: fake.word().capitalize())
+    description = factory.Faker("text", locale="ru_RU")
+    dramatist = factory.RelatedFactory(
+        TeamMemberFactory,
+        factory_related_name="reading",
+        role__name="Драматург",
+    )
+    director = factory.RelatedFactory(
+        TeamMemberFactory,
+        factory_related_name="reading",
+        role__name="Режиссёр",
+    )
+    project = factory.Iterator(Project.objects.all())
+
+
+class ParticipationApplicationFestivalFactory(
+    factory.django.DjangoModelFactory
+):
+    """
+    Create ParticipationApplicationFestival object.
+    """
+
+    class Meta:
+        model = ParticipationApplicationFestival
+
+    first_name = factory.LazyFunction(lambda: fake.first_name())
+    last_name = factory.LazyFunction(lambda: fake.last_name())
+    birthday = factory.LazyFunction(
+        lambda: fake.past_datetime(tzinfo=pytz.UTC)
+    )
+    city = factory.LazyFunction(lambda: fake.city_name())
+    phone_number = factory.LazyFunction(lambda: fake.phone_number())
+    email = factory.LazyFunction(lambda: fake.email())
+    title = factory.LazyFunction(lambda: fake.word().capitalize())
+    year = factory.LazyFunction(lambda: fake.year())
+    file = factory.LazyAttribute(lambda obj: f"{obj.title}.txt")
