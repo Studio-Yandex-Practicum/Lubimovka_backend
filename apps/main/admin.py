e065275d64507a27d86a8c340272d4281ed4bb89
KondratevAD <Kondratev_ad@bk.ru>
2021-12-09 23:52:20 +0300

    add proxy-model setting in main app

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from apps.main.models import Banner
+from apps.core.models import Setting
+from apps.main.models import (
+    Banner,
+    SettingEmail,
+    SettingFirstScreen,
+    SettingGeneral,
+    SettingMain,
+)
 
 
+@admin.register(Banner)
 class BannerAdmin(admin.ModelAdmin):
     list_display = (
         "id",
@@ -12,4 +20,43 @@ class BannerAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Banner, BannerAdmin)
+@admin.register(SettingEmail, SettingGeneral, SettingMain, SettingFirstScreen)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = (
+        "description",
+        "settings_key",
+        "get_value",
+        "group",
+    )
+    search_fields = (
+        "field_type",
+        "settings_key",
+    )
+    readonly_fields = (
+        "field_type",
+        "settings_key",
+        "description",
+    )
+
+    def get_fields(self, request, obj=None):
+        field_for_setting_value = Setting.TYPES_AND_FIELDS[obj.field_type]
+        return (
+            "description",
+            "settings_key",
+            "field_type",
+            "group",
+            field_for_setting_value,
+        )
+
+    @admin.display(description="Значение")
+    def get_value(self, obj: object):
+        """Return value of the setting object."""
+        return obj.value
+
+    def has_add_permission(self, request, obj=None):
+        """Removes the save and add new button."""
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        """Removes the delete button."""
+        return False
