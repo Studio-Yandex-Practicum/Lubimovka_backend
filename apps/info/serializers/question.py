946d02522953e1428898cccadcb53c58a733b960
Konstantin Raikhert <raikhert13@gmail.com>
2021-12-11 23:55:34 +0300

    add autodocumentation for questions

--- apps/info/serializers/question.py
+++ apps/info/serializers/question.py
@@ -4,8 +4,6 @@ from apps.info.models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    author_email = serializers.EmailField()
-
     class Meta:
         model = Question
         fields = "__all__"
--- apps/info/views/question.py
+++ apps/info/views/question.py
@@ -1,9 +1,19 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import generics
 
 from apps.info.serializers.question import QuestionSerializer
 from apps.info.utils import send_question
+from apps.info.views.auto_documentation_info import (
+    ERROR_MESSAGES_FOR_QUESTION_FOR_400,
+)
 
 
+@extend_schema(
+    responses={
+        201: QuestionSerializer,
+        400: ERROR_MESSAGES_FOR_QUESTION_FOR_400,
+    }
+)
 class QuestionCreateAPIView(generics.CreateAPIView):
     serializer_class = QuestionSerializer
 
