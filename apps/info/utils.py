327abe53c3d28080ea7639d5d52e424aa774ceb9
Konstantin Raikhert <raikhert13@gmail.com>
2021-12-12 21:13:42 +0300

    delete email template

--- apps/info/utils.py
+++ apps/info/utils.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
+
+from apps.core.models import Settings
 
 EMAIL_SUBJECT_PREFIX = settings.EMAIL_SUBJECT_PREFIX
 SEND_FROM_QUESTION_EMAIL = settings.SEND_FROM_QUESTION_EMAIL
@@ -8,19 +9,22 @@ SEND_TO_QUESTION_EMAIL = settings.SEND_TO_QUESTION_EMAIL
 
 
 def send_question(serializer):
-    html_message = render_to_string(
-        "email.html",
-        {
-            "question": serializer.validated_data["question"],
-            "author_name": serializer.validated_data["author_name"],
-            "author_email": serializer.validated_data["author_email"],
-        },
-    )
+    """This method transmits question parameters in the template.
+
+    For more information please read the documentation:
+    https://anymail.readthedocs.io/en/stable/esps/mailjet//
+    """
+
     message = EmailMessage(
-        subject=EMAIL_SUBJECT_PREFIX,
-        from_email=SEND_FROM_QUESTION_EMAIL,
-        body=html_message,
-        to=(SEND_TO_QUESTION_EMAIL,),
+        subject=Settings.get_setting("email_subject_for_question"),
+        from_email=Settings.get_setting("email_send_from"),
+        to=Settings.get_setting("mail_send_to"),
     )
-    message.content_subtype = "html"
+
+    message.template_id = (Settings.get_setting("email_subject_for_question"),)
+    message.merge_global_data = {
+        "question": serializer.validated_data["question"],
+        "author_name": serializer.validated_data["author_name"],
+        "author_email": serializer.validated_data["author_email"],
+    }
     message.send()
