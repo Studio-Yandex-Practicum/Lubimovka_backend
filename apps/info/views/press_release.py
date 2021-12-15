e64047a2c93f55d17b93f77dc3bf4a949bb3eb11
Дубинчик Андрей <evil_ghost@bk.ru>
2021-12-16 02:09:24 +0300

    download pdf endpoint

--- apps/info/views/press_release.py
+++ apps/info/views/press_release.py
@@ -1,8 +1,11 @@
 from django.contrib.postgres.aggregates.general import ArrayAgg
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from django.template.loader import get_template
 from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet
+from xhtml2pdf import pisa
 
 from apps.info.filters import YearPressReleaseFilterSet
 from apps.info.models import Festival, PressRelease
@@ -33,3 +36,33 @@ class PressReleaseViewSet(mixins.ListModelMixin, GenericViewSet):
             status=status.HTTP_200_OK,
             json_dumps_params={"ensure_ascii": False},
         )
+
+    @action(
+        detail=False,
+        methods=("get",),
+        url_path=r"(?P<id>[\d]+)/download",
+        url_name="download_press_release",
+        pagination_class=None,
+    )
+    def download_press_release(self, request, **kwargs):
+        press_release = get_object_or_404(
+            PressRelease.objects.select_related("festival"),
+            id=kwargs["id"],
+        )
+        press_release_year = press_release.festival.year
+        response = HttpResponse(content_type="application/pdf")
+        response[
+            "Content-Disposition"
+        ] = f"attachment; filename='press-release_{press_release_year}.pdf'"
+        template = get_template("press_release.html")
+        content = template.render({"press_release": press_release})
+        pisa_status = pisa.CreatePDF(
+            content,
+            dest=response,
+            encoding="UTF-8",
+        )
+        if pisa_status.err:
+            return HttpResponse(
+                "Пожалуйста, попробуйте повторить попытку позже"
+            )
+        return response
