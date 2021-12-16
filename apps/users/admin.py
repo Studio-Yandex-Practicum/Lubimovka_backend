0ce7e9c93f5c38c63380afca6b8f3fa281718a8e
19160393 <PGVasilyeva@sberbank.ru>
2021-12-16 20:27:39 +0300

    Изменение верхнего заголовка на "Администрирование сайта"; Объединение групп и пользователей в одно. Прим: не нашла, как поменять "Пользователи и группы" на "Группы и пользователи"; поменять местами Группу и Пользователей не смогла тоже. Может, посоветуете что-то?

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
+from apps.users.models import ProxyUser
+
 User = get_user_model()
 
 
@@ -9,4 +11,6 @@ class UserAdmin(DjangoUserAdmin):
     list_filter = ["email", "username"]
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(ProxyUser, UserAdmin)
+
+admin.site.site_header = "Администрирование сайта"
--- apps/users/models.py
+++ apps/users/models.py
@@ -5,3 +5,13 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class ProxyUser(User):
+    pass
+
+    class Meta:
+        app_label = "auth"
+        proxy = True
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
