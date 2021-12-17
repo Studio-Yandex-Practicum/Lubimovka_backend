c84804c90dd039a75ca0dadc6c69389e3e4a1db7
Omnomus <bka@inbox.ru>
2021-12-17 18:51:12 +0300

    deleted unnecessary serializers

--- apps/library/serializers/performance.py
+++ apps/library/serializers/performance.py
@@ -1,47 +1,10 @@
 from rest_framework import serializers
 
-from apps.core.models import Role
 from apps.core.serializers import ImageSerializer
 from apps.library.models import Performance, TeamMember
 from apps.library.utilities import team_collector_with_plural_slug
 
 from .play import PlaySerializer
-
-
-class TeamMemberSerializer(serializers.ModelSerializer):
-    id = serializers.SlugRelatedField(
-        source="person",
-        slug_field="id",
-        read_only=True,
-    )
-    full_name = serializers.SlugRelatedField(
-        source="person",
-        slug_field="full_name",
-        read_only=True,
-    )
-
-    class Meta:
-        model = TeamMember
-        fields = (
-            "id",
-            "full_name",
-        )
-
-
-class RoleSerializer(serializers.ModelSerializer):
-    persons = TeamMemberSerializer(
-        source="team_members",
-        read_only=True,
-        many=True,
-    )
-
-    class Meta:
-        model = Role
-        fields = (
-            "name",
-            "slug",
-            "persons",
-        )
 
 
 class PerformanceSerializer(serializers.ModelSerializer):
