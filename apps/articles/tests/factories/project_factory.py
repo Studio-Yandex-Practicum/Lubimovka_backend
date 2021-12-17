9a2106897a71130f72ed0b6c68abb57a80f7ddb3
Omnomus <bka@inbox.ru>
2021-12-17 20:42:09 +0300

    field Intro changed

--- apps/articles/tests/factories/project_factory.py
+++ apps/articles/tests/factories/project_factory.py
@@ -71,7 +71,7 @@ class ProjectFactory(factory.django.DjangoModelFactory):
         variable_nb_sentences=False,
     )
     image = factory.django.ImageField(color=factory.Faker("color"))
-    intro = factory.Faker("text", locale="ru_RU", max_nb_chars=200)
+    intro = factory.Faker("sentence", locale="ru_RU", nb_words=12)
     is_draft = factory.Faker("boolean", chance_of_getting_true=25)
     pub_date = factory.Faker("date_time", tzinfo=timezone.utc)
     title = factory.Faker("text", locale="ru_RU", max_nb_chars=50)
