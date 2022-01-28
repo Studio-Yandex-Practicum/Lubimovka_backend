6a3e2fd1c7aabdd9b4ef333cccfab6b212aa3f9d
Omnomus <bka@inbox.ru>
2022-01-28 15:05:51 +0300

    squash users migrations

--- apps/users/migrations/0002_set_default_user_roles.py
+++ apps/users/migrations/0002_set_default_user_roles.py
@@ -11,21 +11,6 @@ def set_default_groups(apps, schema_editor):
             Group(name="editor"),
         ]
     )
-    Permission = apps.get_model("auth", "Permission")
-    permissions = Permission.objects.filter(
-        Q(codename__endswith="event")
-        | Q(codename__endswith="blogitem")
-        | Q(codename__endswith="newsitem")
-        | Q(codename__endswith="project")
-        | Q(codename__endswith="image")
-        | Q(codename__endswith="performance")
-        | Q(codename__endswith="play")
-    ).exclude(name__contains="delete")
-
-    admin = Group.objects.get(name="admin")
-    admin.permissions.add(*Permission.objects.all())
-    editor = Group.objects.get(name="editor")
-    editor.permissions.add(*permissions)
 
 
 class Migration(migrations.Migration):
