e336f743331c260feba32f44066fc622ac93217e
Viktor Lebedev <rs.porosenok@gmail.com>
2022-01-31 01:22:04 +0300

    add facebook link to General Settings

--- apps/core/migrations/0002_setting_initial_data.py
+++ apps/core/migrations/0002_setting_initial_data.py
@@ -310,6 +310,14 @@ def add_general_settings(apps, schema_editor):
         description="Форма для отправки пьесы",
     )
 
+    Setting.objects.create(
+        field_type="URL",
+        group="GENERAL",
+        settings_key="photo_gallery_facebook",
+        url="https://www.facebook.com/festival.lubimovka/photos",
+        description="Ссылка на фотоальбомы в Facebook на странице для прессы",
+    )
+
 def add_main_settings(apps, schema_editor):
 
     Setting = apps.get_model("core", "Setting")
