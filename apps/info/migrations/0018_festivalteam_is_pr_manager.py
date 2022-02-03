9725a3aa87a833d47b58e9c0b99503d8fabed316
Степан Киселев <Stepakiselev@mail.ru>
2022-02-03 13:22:14 +0600

    add pr-manager

--- apps/info/migrations/0018_festivalteam_is_pr_manager.py
+++ apps/info/migrations/0018_festivalteam_is_pr_manager.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('info', '0017_alter_place_description'),
+        ('info', '0001_initial'),
     ]
 
     operations = [
