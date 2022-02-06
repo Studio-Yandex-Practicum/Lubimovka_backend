0cb96afcea0ea687a5710d65f5beab93d5151dd8
nkiryanov <70921439+nkiryanov@users.noreply.github.com>
2022-02-06 23:03:50 +0300

    Bugfix/person factories (#227)
    * Partner factory doesn't require internet now.
    * Some factories refactoring

--- apps/main/tests/factories.py
+++ apps/main/tests/factories.py
@@ -1,7 +1,6 @@
-import urllib
-
 import factory
-from django.core.files.base import ContentFile
+
+from apps.core.utils import get_picsum_image
 
 from ..models import Banner
 
@@ -11,25 +10,22 @@ class BannerFactory(factory.django.DjangoModelFactory):
 
     No more than 3 banner objects could be created. The factory couldn't be
     used to update existed objects.
+    The behavior is different based on param:
+        - `add_real_image`: create Banner with real image. Requires internet.
     """
 
     class Meta:
         model = Banner
         django_get_or_create = ("id",)
 
-    id = factory.Iterator(range(3))
+    id = factory.Iterator(range(1, 4))
     title = factory.Faker("sentence", nb_words=6, locale="ru_RU")
     description = factory.Faker("sentence", nb_words=16, locale="ru_RU")
     url = factory.Faker("url")
     image = factory.django.ImageField(color=factory.Faker("color"))
     button = factory.Iterator(Banner.ButtonType.values)
 
-    @factory.post_generation
-    def add_real_image(self, created, extracted, **kwargs):
-        """Set image from picsum.photos. Requires internet connection.
-
-        To use it set `add_real_image=True`
-        """
-        if extracted is True:
-            image = urllib.request.urlopen("https://picsum.photos/800/800").read()
-            self.image.save(self.title + ".jpg", ContentFile(image), save=False)
+    class Params:
+        add_real_image = factory.Trait(
+            image=factory.django.ImageField(from_func=get_picsum_image),
+        )
