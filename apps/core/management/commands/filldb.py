201262d9270884f6b125a80ea63315678924074a
nkiryanov <70921439+nkiryanov@users.noreply.github.com>
2022-02-07 17:33:41 +0300

    Рефакторинг фабрик пользователя (UserFactory) и приложения users (#231)
    UserFactories:
    1. Переместил из приложения core в приложения users, где они и должны были быть
    2. Изменил чуть логику создания: теперь обязательно указывать тип группы которую создаем (нам не нужны в фабриках пользователи с правами кроме как `editor` и `admin`. Теперь если группа не указана пользователь не создастся. Проверка происходит на момент вызова. То есть сначала проверяем, что параметры заданы, а потом вызываем.
    3. Для удобства сделал две отдельные фабрики: EditorUserFactory и AdminUserFactory
    4. Переписал filldb и убрал циклы где это возможно. Убрал логику назначения username и перенёс её в фабрику.
    
    Приложение Users:
    1. Мы регистрировали нашу модель пользователя в приложении `auth`, которое системное. То есть создавалась миграция в папке приложения, которая не хранится в нашем коде. Не знаю на каком этапе мы это пропустили, но я такое не ревьювил 🙈. Перенёс регистрацию групп в приложение users.
    2. Разложил по папкам так, как это должно быть: отключаем стандартную регистрацию групп в приложении core, регистрируем новую группу в приложении users.

--- apps/core/management/commands/filldb.py
+++ apps/core/management/commands/filldb.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from apps.afisha.tests.factories import EventFactory
 from apps.content_pages.tests.factories import ImageForContentFactory, VideoFactory
-from apps.core.tests.factories import ImageFactory, PersonFactory, UserFactory
+from apps.core.tests.factories import ImageFactory, PersonFactory
 from apps.info.tests.factories import (
     FestivalFactory,
     FestivalTeamFactory,
@@ -24,6 +24,7 @@ from apps.library.tests.factories import (
     ReadingFactory,
 )
 from apps.main.tests.factories import BannerFactory as MainBannerFactory
+from apps.users.tests.factories import AdminUserFactory, EditorUserFactory
 
 
 def notification(command, objects, text):
@@ -61,29 +62,25 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         try:
             persons_base = PersonFactory.create_batch(30)
-            persons_with_image = []
-            persons_with_image_email_city = []
-            for _ in range(50):
-                persons_with_image.append(PersonFactory.create(add_real_image=True))
-                persons_with_image_email_city.append(
-                    PersonFactory(
-                        add_real_image=True,
-                        add_email=True,
-                        add_city=True,
-                    )
-                )
             notification(self, persons_base, "базовых персон")
+
+            persons_with_image = PersonFactory.create_batch(30, add_real_image=True)
             notification(self, persons_with_image, "персоны с фото")
-            notification(
-                self,
-                persons_with_image_email_city,
-                "персон с фото, городом, email",
+
+            persons_with_image_email_city = PersonFactory.create_batch(
+                30,
+                add_real_image=True,
+                add_email=True,
+                add_city=True,
             )
+            notification(self, persons_with_image_email_city, "персон с фото, городом, email")
+
             partners = PartnerFactory.create_batch(30)
             notification(self, partners, "партнёров")
 
             in_footer_partners = PartnerFactory.create_batch(
                 5,
+                add_real_image=True,
                 type="general",
                 in_footer_partner=True,
             )
@@ -113,15 +110,13 @@ class Command(BaseCommand):
             videos_url = VideoFactory.create_batch(5)
             notification(self, videos_url, "видео")
 
-            users_editors = []
-            users_admins = []
-            for index in range(1, 6):
-                users_editors.append(UserFactory.create(username=f"editor_{index}", add_role_editor=True))
-                users_admins.append(UserFactory.create(username=f"admin_{index}", add_role_admin=True))
+            users_editors = AdminUserFactory.create_batch(5)
             notification(self, users_editors, "редакторов")
+
+            users_admins = EditorUserFactory.create_batch(5)
             notification(self, users_admins, "админов")
 
-            # <Library factories>
+            # Library factories.
 
             programtypes = ProgramFactory.create_batch(3)
             notification(self, programtypes, "программ")
