64c4f65c3b125d0816fa198ecc214815dca0bc78
Konstantin Raikhert <raikhert13@gmail.com>
2022-02-19 19:40:40 +0300

    change setiings of the django degug toolbar for docker use

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -26,8 +26,22 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_COLLAPSED": True,
     "SHOW_TEMPLATE_CONTEXT": True,
-    "SHOW_TOOLBAR_CALLBACK": lambda _request: DEBUG,
 }
+
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+# Add in INTERNAL_IPS internal IP of the docker container if DEGUG == True.
+if env("DEBUG"):
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+    try:
+        _, _, ips = socket.gethostbyname_ex("node")
+        INTERNAL_IPS.extend(ips)
+    except socket.gaierror:
+        # The node container isn't started (yet?)
+        pass
 
 # Use PostgreSQL
 # ------------------------------------------------------------------------------
