f4bf6cdba74be1f7f68a6698c989fc2f0712ffe8
Nikolay <nikolay@kiryanov.ru>
2022-02-20 02:25:51 +0300

    Update and refactor: 1. pub_date on BlogItem and NewsItem is required field 2. BlogItemListApi and BlogItemRetrieveApi are rewritten. Serializers are also rewritten 3. !!! Ordering was removed from BlogItem Apis. Not sure that it was needed

--- apps/articles/serializers/blog_items.py
+++ apps/articles/serializers/blog_items.py
@@ -1,25 +1,17 @@
-from django.db.models.query import Prefetch
-from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 
 from apps.articles.models import BlogItem
-from apps.articles.schema.schema_extension import SUCCESS_MESSAGE_FOR_BLOG_ITEM_DETAIL_FOR_200
-from apps.articles.services import get_latest_four_published_items_data
-from apps.content_pages.serializers import BaseContentPageSerializer
 from apps.core.models import Role
 
 
 class RoleSerializer(serializers.ModelSerializer):
 
-    persons = serializers.SerializerMethodField()
-
-    def get_persons(self, obj):
-
-        persons = []
-
-        for team_member in obj.blog_persons.all():
-            persons.append(team_member.person.full_name)
-        return persons
+    persons = serializers.SlugRelatedField(
+        source="blog_persons",
+        slug_field="annotated_full_name",
+        many=True,
+        read_only=True,
+    )
 
     class Meta:
         model = Role
@@ -30,7 +22,9 @@ class RoleSerializer(serializers.ModelSerializer):
         )
 
 
-class BlogItemBaseSerializer(serializers.ModelSerializer):
+class BlogItemListSerializer(serializers.ModelSerializer):
+    pub_date = serializers.DateTimeField(required=True)
+
     class Meta:
         model = BlogItem
         fields = (
@@ -41,89 +35,4 @@ class BlogItemBaseSerializer(serializers.ModelSerializer):
             "author_url",
             "author_url_title",
             "image",
-        )
-
-
-class BlogItemListSerializer(BlogItemBaseSerializer):
-    pass
-
-
-@extend_schema_serializer(
-    examples=[
-        OpenApiExample(
-            name="Schema for blog item detail",
-            value=SUCCESS_MESSAGE_FOR_BLOG_ITEM_DETAIL_FOR_200,
-            request_only=False,
-            response_only=True,
-        ),
-    ],
-)
-class BlogItemDetailedSerializer(
-    BaseContentPageSerializer,
-    serializers.ModelSerializer,
-):
-    other_blogs = serializers.SerializerMethodField()
-    team = serializers.SerializerMethodField()
-
-    def get_other_blogs(self, obj):
-        """Return latest four `BlogItem` except the object itself."""
-        serialized_data = get_latest_four_published_items_data(
-            serializer_class=BlogItemListSerializer,
-            object=obj,
-        )
-        return serialized_data
-
-    def get_team(self, obj):
-        """Make `team` serialized data based on `roles` and `blog_persons`.
-
-        Team serialized data has to look like this:
-        "team": [
-            {
-                "name": "Переводчик",
-                "slug": "translator",
-                "persons": [
-                    "Каллистрат Абрамов",
-                    "Александра Авдеева"
-            },
-            {
-                ...
-            }
-        ]
-
-        To make it three things have to be done:
-            1. Limit `roles` to distinct `roles`
-            2. Limit (prefetch) `blog_persons` (roles reverse relation) with
-            blog_item's objects only (typically `role.blog_persons` returns all
-            blog_persons, not only related to exact blog_item).
-            3. Return serialized data
-        """
-        blog_item = obj
-        blog_roles = blog_item.roles.distinct()
-        blog_persons = blog_item.blog_persons.all()
-
-        blog_roles_with_limited_blog_persons = blog_roles.prefetch_related(
-            Prefetch(
-                "blog_persons",
-                queryset=blog_persons,
-            ),
-        )
-        serializer = RoleSerializer(
-            instance=blog_roles_with_limited_blog_persons,
-            many=True,
-        )
-        return serializer.data
-
-    class Meta:
-        model = BlogItem
-        fields = (
-            "id",
-            "title",
-            "description",
-            "image",
-            "author_url",
-            "author_url_title",
-            "pub_date",
-            "contents",
-            "team",
-            "other_blogs",
         )
--- apps/core/utils.py
+++ apps/core/utils.py
@@ -2,6 +2,7 @@ import urllib
 
 from django.core.files.base import ContentFile
 from django.template.defaultfilters import slugify as django_slugify
+from rest_framework.response import Response
 
 from apps.core.constants import ALPHABET
 
@@ -15,3 +16,16 @@ def get_picsum_image(width: int = 1024, height: int = 768) -> ContentFile:
     """Return real image from picsum.photos. Supports width and height arguments."""
     image = urllib.request.urlopen(f"https://picsum.photos/{width}/{height}").read()
     return ContentFile(image)
+
+
+def get_paginated_response(pagination_class, serializer_class, queryset, request, view):
+    """Return paginated response. Use it with `list` views based on APIView."""
+    paginator = pagination_class()
+    page = paginator.paginate_queryset(queryset, request, view=view)
+
+    if page is not None:
+        serializer = serializer_class(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    serializer = serializer_class(queryset, many=True)
+    return Response(data=serializer.data)
