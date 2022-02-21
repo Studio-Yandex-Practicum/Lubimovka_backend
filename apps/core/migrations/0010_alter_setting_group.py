0064fe56a89afabfadcf5ed67f5e6cb196d293f5
Omnomus <bka@inbox.ru>
2022-02-21 16:52:59 +0300

    rename migration

--- apps/core/migrations/0010_alter_setting_group.py
+++ apps/core/migrations/0010_alter_setting_group.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_settings_change_data'),
+        ('core', '0009_alter_image_image'),
     ]
 
     operations = [
