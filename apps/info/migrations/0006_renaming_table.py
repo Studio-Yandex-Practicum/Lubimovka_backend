a8f736cc4d6dc3b0e729399e6d0a52fbfcb25a5f
baby-platom <platon200502@gmail.com>
2022-02-26 11:54:42 +0600

    final

--- apps/info/migrations/0006_renaming_table.py
+++ apps/info/migrations/0006_renaming_table.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('info', '005_festivalteam_is_pr_manager.py'),
+        ('info', '005_festivalteam_is_pr_manager'),
     ]
 
     operations = [
