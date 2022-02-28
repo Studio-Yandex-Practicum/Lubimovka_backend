b28db3276e9191fdd118f34118d936255d249c04
Omnomus <bka@inbox.ru>
2022-02-28 15:48:46 +0300

    get setting by setting.get_setting

--- apps/library/permissions.py
+++ apps/library/permissions.py
@@ -6,6 +6,6 @@ from apps.core.models import Setting
 
 class SettingsPlayReceptionPermission(BasePermission):
     def has_permission(self, request, view):
-        if not Setting.objects.get(settings_key="plays_reception_is_open").boolean:
+        if not Setting.get_setting("plays_reception_is_open").boolean:
             raise PermissionDenied("Приём пьес закрыт.")
         return True
