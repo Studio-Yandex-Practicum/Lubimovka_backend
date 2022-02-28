60be44bec631ca4869556b7f657c11dbe863beb5
Дубинчик Андрей <evil_ghost@bk.ru>
2022-03-01 02:06:05 +0300

    move logic or getting team members to model properties

--- apps/library/serializers/masterclass.py
+++ apps/library/serializers/masterclass.py
@@ -1,27 +1,14 @@
-from typing import List
-
-from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from apps.library.models import MasterClass
-from apps.library.serializers.utilities import TeamTypedDict, get_event_team_roles, get_event_team_serialized_data
+from apps.library.serializers.role import RoleSerializer
 
 
 class EventMasterClassSerializer(serializers.ModelSerializer):
     """Master-class serializer for afisha page."""
 
-    team = serializers.SerializerMethodField()
+    team = RoleSerializer(source="event_team", many=True)
     project_title = serializers.SlugRelatedField(slug_field="title", read_only=True, source="project")
-
-    @extend_schema_field(List[TeamTypedDict])
-    def get_team(self, obj):
-        """Collect team in two stages.
-
-        First select roles and persons related with role and event.
-        Then pack it to the dictionary and add to list.
-        """
-        roles = get_event_team_roles(obj, {"team_members__masterclass": obj, "slug__in": ["host"]})
-        return get_event_team_serialized_data(roles)
 
     class Meta:
         model = MasterClass
