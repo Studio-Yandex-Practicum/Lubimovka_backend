0fb3e48263b757de9296a47f5165904b62001bb7
baby-platom <platon200502@gmail.com>
2022-03-02 22:42:59 +0600

    fix of migration's name

--- apps/info/migrations/0009_ordering_of_people.py
+++ apps/info/migrations/0009_ordering_of_people.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('info', '0007_renaming_table'),
+        ('info', '0008_alter_partner_image'),
     ]
 
     operations = [
