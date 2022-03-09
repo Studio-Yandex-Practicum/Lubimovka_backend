a667a1c2f69117f6a153334b76b60789962cfcc6
Omnomus <bka@inbox.ru>
2022-03-09 08:58:17 +0300

    fixed error message in form

--- apps/library/forms/admin.py
+++ apps/library/forms/admin.py
@@ -10,6 +10,6 @@ class AuthorForm(forms.ModelForm):
         slug = slugify(full_name)
         self.fields["slug"].error_messages = {
             "unique": f"Автоматически сформированный транслит '{slug}' уже используется,"
-            "необходимо ввести оригинальный вручную",
+            " необходимо ввести оригинальный вручную",
         }
         return super().clean()
