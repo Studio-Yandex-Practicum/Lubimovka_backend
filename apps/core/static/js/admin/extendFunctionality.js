7a22235171365ad87bba972bae1c2bd445c4e7ff
Степан Киселев <Stepakiselev@mail.ru>
2022-03-13 21:56:11 +0600

    refactor placeholderInSearch.js to extendFunctionality.js

--- apps/core/static/js/admin/extendFunctionality.js
+++ apps/core/static/js/admin/extendFunctionality.js
@@ -1,3 +1,5 @@
+// add place holder in search field
+// add autoscrolling to side panel
 window.onload = function() {
     document.getElementsByClassName("current-app")[0].scrollIntoView();
     var searchbar = document.getElementById("searchbar");
