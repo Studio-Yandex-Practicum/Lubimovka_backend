50f8e7a91f9ddf6003bfab497972ae0e993a927d
unknown <huppenen.ad@gmail.com>
2022-03-18 23:41:32 +0300

    updated field phone_number

--- apps/library/factories/participationapplication.py
+++ apps/library/factories/participationapplication.py
@@ -1,9 +1,23 @@
 import factory
+import phonenumbers
 from faker import Faker
+from faker.providers.phone_number.ru_RU import Provider
 
 from apps.library.models import UNIQUE_CONSTRAINT_FIELDS_FOR_PARTICIPATION, ParticipationApplicationFestival
 
 fake = Faker("ru_RU")
+
+
+class CustomPhoneProvider(Provider):
+    def phone_number(self):
+        while True:
+            phone_number = self.numerify(self.random_element(self.formats))
+            parsed_number = phonenumbers.parse(phone_number, "RU")
+            if phonenumbers.is_valid_number(parsed_number):
+                return phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
+
+
+fake.add_provider(CustomPhoneProvider)
 
 
 class ParticipationApplicationFestivalFactory(factory.django.DjangoModelFactory):
@@ -17,7 +31,7 @@ class ParticipationApplicationFestivalFactory(factory.django.DjangoModelFactory)
     last_name = factory.Faker("last_name", locale="ru_RU")
     birth_year = factory.Faker("year")
     city = factory.Faker("city_name", locale="ru_RU")
-    phone_number = factory.Faker("phone_number")
+    phone_number = factory.LazyAttribute(lambda _: fake.phone_number())
     email = factory.Faker("email")
     title = factory.LazyFunction(lambda: fake.word().capitalize())
     year = factory.Faker("year")
