1a498f0f400b9aa74eef4170c05f5d0346e91a84
baby-platom <platon200502@gmail.com>
2022-03-19 23:12:35 +0600

    first step

--- apps/library/utilities.py
+++ apps/library/utilities.py
@@ -1,3 +1,6 @@
+import os
+
+import yadisk
 from django.db.models.query import Prefetch
 from django.utils import timezone
 
@@ -23,3 +26,21 @@ def get_team_roles(obj, filters: dict = None):
     roles = Role.objects.filter(**filters).distinct()
     team = obj.team_members.all()
     return roles.prefetch_related(Prefetch("team_members", team))
+
+
+def yandex_disk_export(y, instance):
+    cwd = str(os.getcwd())
+    name = str(instance.file).split("/")[-1]
+    name = name.replace("\\", "_").replace("/", "_")
+    try:
+        y.mkdir(f"/{str(instance.year)}")
+    except yadisk.exceptions.PathExistsError:
+        pass
+    to_dir = f"/{str(instance.year)}/{name}"
+    from_dir = cwd.replace("\\", "/") + "/media/" + str(instance.file)
+    try:
+        y.upload(from_dir, to_dir)
+    except yadisk.exceptions.PathExistsError:
+        pass
+    print(y.exists(f"/{str(instance.year)}"))
+    print(y.copy(f"/{str(instance.year)}"))
--- apps/library/views/participation.py
+++ apps/library/views/participation.py
@@ -1,5 +1,6 @@
 import logging
 
+import yadisk
 from drf_spectacular.utils import extend_schema
 from googleapiclient.errors import HttpError
 from rest_framework import mixins, viewsets
@@ -11,12 +12,14 @@ from apps.library.schema.schema_extension import (
 )
 from apps.library.serializers.participation import ParticipationSerializer
 from apps.library.services.spreadsheets import GoogleSpreadsheets
+from apps.library.utilities import yandex_disk_export
 from config.logging import LOGGING_CONFIG
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
 gs = GoogleSpreadsheets()
+y = yadisk.YaDisk(token="AQAAAAAoiXEIAAfBYfQtH0Dmg05Zpw0i8B-_1_Y")
 
 
 @extend_schema(
@@ -33,6 +36,24 @@ class ParticipationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     def perform_create(self, serializer):
         instance = serializer.save()
         domain = self.request.build_absolute_uri()
+
+        yandex_disk_export(y, instance)
+        # cwd = str(os.getcwd())
+        # name = str(instance.file).split("/")[-1]
+        # name = name.replace("\\", '_').replace("/", '_')
+        # try:
+        #     y.mkdir(f"/{str(instance.year)}")
+        # except yadisk.exceptions.PathExistsError:
+        #     pass
+        # to_dir = f"/{str(instance.year)}/{name}"
+        # from_dir = cwd.replace("\\", '/') + "/media/" + str(instance.file)
+        # try:
+        #     y.upload(from_dir, to_dir)
+        # except yadisk.exceptions.PathExistsError:
+        #     pass
+        # print(y.exists(f"/{str(instance.year)}"))
+        # print(y.copy(f"/{str(instance.year)}"))
+
         try:
             export_success = gs.export(instance=instance, domain=domain)
             if export_success:
