fce1ce291435261eb5e089cb28649e0e54fab54b
NiKuma0 <ybr811@gmail.com>
2022-03-23 03:25:45 +0300

    flake8

--- apps/library/forms/admin_forms.py
+++ apps/library/forms/admin_forms.py
@@ -2,14 +2,7 @@ from django.forms.models import BaseInlineFormSet
 
 
 class RequiredInlineFormSet(BaseInlineFormSet):
-    """
-    Generates an inline formset that is required
-    """
-
     def _construct_form(self, i, **kwargs):
-        """
-        Override the method to change the form attribute empty_permitted
-        """
         form = super(RequiredInlineFormSet, self)._construct_form(i, **kwargs)
         form.empty_permitted = False
         return form
