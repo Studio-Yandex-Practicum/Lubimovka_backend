2443779360b5269ad2eaba0920982b2b52b0b97a
Konstantin Raikhert <raikhert13@gmail.com>
2022-03-28 02:02:53 +0300

    add forbitten and letters for authors

--- apps/library/schema/schema_extension.py
+++ apps/library/schema/schema_extension.py
@@ -38,3 +38,5 @@ ERROR_MESSAGES_FOR_PARTICIPATION_FOR_400 = {
     }
 }
 ERROR_MESSAGES_FOR_PARTICIPATION_FOR_403 = {"example": {"detail": "Приём пьес закрыт."}}
+
+ERROR_MESSAGES_FOR_AUTHOR_FOR_403 = {"example": {"detail": "Укажите параметр - letter."}}
--- apps/library/views/author.py
+++ apps/library/views/author.py
@@ -1,17 +1,33 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.library.filters import AuthorFilter
 from apps.library.models import Author
-from apps.library.serializers import AuthorListSerializer, AuthorRetrieveSerializer
+from apps.library.schema.schema_extension import ERROR_MESSAGES_FOR_AUTHOR_FOR_403
+from apps.library.serializers import AuthorLettersSerializer, AuthorListSerializer, AuthorRetrieveSerializer
 
 
+@extend_schema(
+    responses={
+        201: AuthorListSerializer,
+        403: ERROR_MESSAGES_FOR_AUTHOR_FOR_403,
+    }
+)
 class AuthorsReadViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Author.objects.select_related("person").all()
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AuthorFilter
     lookup_field = "slug"
+
+    def list(self, request):
+        if self.action == "list" and not request.query_params.get("letter"):
+            raise PermissionDenied("Укажите параметр - letter.")
+        return super().list(self, request)
 
     def get_object(self):
         author = get_object_or_404(
@@ -30,3 +46,16 @@ class AuthorsReadViewSet(viewsets.ReadOnlyModelViewSet):
         if self.action == "retrieve":
             return AuthorRetrieveSerializer
         return AuthorListSerializer
+
+
+class AuthorLettersAPIView(APIView):
+    """Returns a list of the first letters of the available authors' surnames."""
+
+    @extend_schema(responses=AuthorLettersSerializer)
+    def get(self, request):
+        authors_list = Author.objects.select_related("person").all()
+        letters_values_list = [author.person.last_name[0] for author in authors_list]
+        letters_values_list_without_double = sorted(list(set(letters_values_list)))
+        letters_instance = {"letters": letters_values_list_without_double}
+        serializer = AuthorLettersSerializer(instance=letters_instance)
+        return Response(serializer.data)
