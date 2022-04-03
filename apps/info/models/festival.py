2d0f4fd4488d01f8f5616ddb9c1c6cbf02983642
NiKuma0 <ybr811@gmail.com>
2022-04-03 16:40:16 +0300

    Test: change model wthout makemigrations

--- apps/info/models/festival.py
+++ apps/info/models/festival.py
@@ -151,6 +151,7 @@ class Festival(BaseModel):
         blank=True,
         verbose_name="Изображение для страницы пресс-релизов",
     )
+    test = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = "Фестиваль"
