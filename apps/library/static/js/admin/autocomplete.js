1bca34e2860f81960da645b3dfc6f1a787c30f2a
Степан Киселев <Stepakiselev@mail.ru>
2022-04-05 00:06:47 +0300

    update authocomplete.js

--- apps/library/static/js/admin/autocomplete.js
+++ apps/library/static/js/admin/autocomplete.js
@@ -1,64 +1,38 @@
+// function adds select2
+function setSelect2($, $objectId, $placeHolder) {
+    const $addAllowClear = true;
+    const $addWidth = "180px";
+    return $($objectId).select2({
+        placeholder: $placeHolder,
+        allowClear: $addAllowClear,
+        width: $addWidth,
+    })
+};
+
 $(document).ready(function() {
-    $("#id_common_event").select2({
-        placeholder: "Выберите событие",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Author_achievements-0-achievement").select2({
-        placeholder: "Выберите достижение",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Author_plays-0-play").select2({
-        placeholder: "Выберите пьесу",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Author_plays-2-0-play").select2({
-        placeholder: "Выберите пьесу",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_social_networks-0-name").select2({
-        placeholder: "Выберите соцсеть",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Author_plays-0-author").select2({
-        placeholder: "Выберите человека",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Author_plays-1-author").select2({
-        placeholder: "Выберите человека",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_volunteers-0-person").select2({
-        placeholder: "Выберите человека",
-        allowClear: true,
-        width: "180px",
-    });
-    $(".field-role select").select2({
-        placeholder: "Выберите роль",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Performance_images_in_block-0-image").select2({
-        placeholder: "Выберите изображение",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_Festival_images-0-image").select2({
-        placeholder: "Выберите изображение",
-        allowClear: true,
-        width: "180px",
-    });
+    setSelect2($, "select#id_project", "Выберите проект");
+    setSelect2($, "select#id_person", "Выберите человека");
+    setSelect2($, "select#id_performance", "Выберите спектакль");
+    setSelect2($, "select#id_program", "Выберите программу");
+    setSelect2($, "select#id_festival", "Выберите фестиваль");
+    setSelect2($, "select#id_play", "Выберите пьесу");
+    setSelect2($, "select#id_type", "Выберите тип");
+    setSelect2($, "select#id_button", "Выберите тип кнопки");
+    setSelect2($, "#id_common_event", "Выберите событие");
+    setSelect2($, "#id_Author_achievements-0-achievement", "Выберите достижение");
+    setSelect2($, "select#id_Author_plays-0-play", "Выберите пьесу");
+    setSelect2($, "select#id_Author_plays-2-0-play", "Выберите пьесу");
+    setSelect2($, "select#id_social_networks-0-name", "Выберите соцсеть");
+    setSelect2($, "select#id_Author_plays-0-author", "Выберите человека");
+    setSelect2($, "select#id_Author_plays-1-author", "Выберите человека");
+    setSelect2($, "select#id_volunteers-0-person", "Выберите человека");
+    setSelect2($, "select#id_Performance_images_in_block-0-image", "Выберите изображение");
+    setSelect2($, "select#id_Festival_images-0-image", "Выберите изображение");
 
     // trigger to add new elements to tree house
     $('tbody').bind("DOMNodeInserted",function(event){
-        let element = event.target.querySelector("select");
-        var obj = {
+        let elements = event.target.querySelectorAll("select");
+        var $obj = {
             "author": "человека",
             "achievement": "достижение",
             "play": "пьесу",
@@ -68,59 +42,22 @@ $(document).ready(function() {
             "role": "роль",
             "content_type": "тип объкта",
         };
-        if (element) {
-            let lastWord = element.id.split("-").slice(-1);
-            if (lastWord in obj) {
-                placeHolderWord = obj[lastWord]
+        var element;
+        if (elements) {
+            for (element of elements) {
+                let lastWord = element.id.split("-").slice(-1);
+                if (lastWord in $obj) {
+                    placeHolderWord = $obj[lastWord]
+                    }
+                else {
+                    placeHolderWord = "..."
                 }
-            else {
-                placeHolderWord = "..."
+                $("select#"+element.id).select2({
+                    placeholder: "Выберите " + placeHolderWord,
+                    allowClear: true,
+                    width: "180px",
+                });
             }
-            $("#"+element.id).select2({
-                placeholder: "Выберите " + placeHolderWord,
-                allowClear: true,
-                width: "180px",
-            });
         }
-    });
-    $("#id_project").select2({
-        placeholder: "Выберите проект",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_person").select2({
-        placeholder: "Выберите человека",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_performance").select2({
-        placeholder: "Выберите спектакль",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_program").select2({
-        placeholder: "Выберите программу",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_festival").select2({
-        placeholder: "Выберите фестиваль",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_play").select2({
-        placeholder: "Выберите пьесу",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_type").select2({
-        placeholder: "Выберите тип",
-        allowClear: true,
-        width: "180px",
-    });
-    $("#id_button").select2({
-        placeholder: "Выберите тип кнопки",
-        allowClear: true,
-        width: "180px",
     });
 });
