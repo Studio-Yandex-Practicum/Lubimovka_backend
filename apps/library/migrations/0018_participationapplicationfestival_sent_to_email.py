69e8a6da6614d2ee4a44a17e02646050f6267e5a
Michael M <trdMichaelM@yandex.ru>
2022-04-04 19:07:03 +0300

    Created new migration for sent_to_email

--- apps/library/migrations/0018_participationapplicationfestival_sent_to_email.py
+++ apps/library/migrations/0018_participationapplicationfestival_sent_to_email.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('library', '0015_alter_author_slug'),
+        ('library', '0017_delete_other_play'),
     ]
 
     operations = [
