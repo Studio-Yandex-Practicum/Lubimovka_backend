26a81c5fb2c953aff156ce9464e8be2d495aa731
Степан Киселев <Stepakiselev@mail.ru>
2022-04-04 18:40:16 +0300

    fix authocomplete.js

--- apps/library/static/js/admin/autocomplete.js
+++ apps/library/static/js/admin/autocomplete.js
@@ -4,22 +4,37 @@ $(document).ready(function() {
         allowClear: true,
         width: "180px",
     });
-    $(".field-achievement select").select2({
+    $("#id_Author_achievements-0-achievement").select2({
         placeholder: "Выберите достижение",
         allowClear: true,
         width: "180px",
     });
-    $(".field-play select").select2({
+    $("#id_Author_plays-0-play").select2({
         placeholder: "Выберите пьесу",
         allowClear: true,
         width: "180px",
     });
-    $(".field-author select").select2({
+    $("#id_Author_plays-2-0-play").select2({
+        placeholder: "Выберите пьесу",
+        allowClear: true,
+        width: "180px",
+    });
+    $("#id_social_networks-0-name").select2({
+        placeholder: "Выберите соцсеть",
+        allowClear: true,
+        width: "180px",
+    });
+    $("#id_Author_plays-0-author").select2({
         placeholder: "Выберите человека",
         allowClear: true,
         width: "180px",
     });
-    $(".field-person select").select2({
+    $("#id_Author_plays-1-author").select2({
+        placeholder: "Выберите человека",
+        allowClear: true,
+        width: "180px",
+    });
+    $("#id_volunteers-0-person").select2({
         placeholder: "Выберите человека",
         allowClear: true,
         width: "180px",
@@ -29,32 +44,39 @@ $(document).ready(function() {
         allowClear: true,
         width: "180px",
     });
-    $(".field-image select").select2({
+    $("#id_Performance_images_in_block-0-image").select2({
+        placeholder: "Выберите изображение",
+        allowClear: true,
+        width: "180px",
+    });
+    $("#id_Festival_images-0-image").select2({
         placeholder: "Выберите изображение",
         allowClear: true,
         width: "180px",
     });
 
+    // trigger to add new elements to tree house
     $('tbody').bind("DOMNodeInserted",function(event){
-        let el = event.target.querySelector("select");
+        let element = event.target.querySelector("select");
         var obj = {
             "author": "человека",
             "achievement": "достижение",
             "play": "пьесу",
+            "name": "соцсеть",
             "image": "изображение",
             "person": "человека",
             "role": "роль",
             "content_type": "тип объкта",
         };
-        if (el) {
-            let lastWord = el.id.split("-").slice(-1);
+        if (element) {
+            let lastWord = element.id.split("-").slice(-1);
             if (lastWord in obj) {
                 placeHolderWord = obj[lastWord]
                 }
             else {
                 placeHolderWord = "..."
             }
-            $("#"+el.id).select2({
+            $("#"+element.id).select2({
                 placeholder: "Выберите " + placeHolderWord,
                 allowClear: true,
                 width: "180px",
@@ -63,6 +85,11 @@ $(document).ready(function() {
     });
     $("#id_project").select2({
         placeholder: "Выберите проект",
+        allowClear: true,
+        width: "180px",
+    });
+    $("#id_person").select2({
+        placeholder: "Выберите человека",
         allowClear: true,
         width: "180px",
     });
