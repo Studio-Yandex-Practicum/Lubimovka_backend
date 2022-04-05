0866c4f1abd7a6039c1964efcc9bf2b4121ef207
Michael M <trdMichaelM@yandex.ru>
2022-04-05 11:25:54 +0300

    fix migrate conflict

--- apps/library/migrations/0019_participationapplicationfestival_sent_to_email.py
+++ apps/library/migrations/0019_participationapplicationfestival_sent_to_email.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('library', '0017_delete_other_play'),
+        ('library', '0018_participationapplicationfestival_url_file_in_storage'),
     ]
 
     operations = [
