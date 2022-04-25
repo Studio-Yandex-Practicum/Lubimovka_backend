1fa5bc2d2371dd7f34d0dc6d0e0fd157e8c34c78
baby-platom <platon200502@gmail.com>
2022-04-25 22:55:31 +0600

    upgrades

--- apps/feedback/admin/__init__.py
+++
@@ -1,7 +0,0 @@
-from .participation import ParticipationAdmin
-from .question import QuestionAdmin
-
-__all__ = (
-    QuestionAdmin,
-    ParticipationAdmin,
-)
--- apps/feedback/admin/question.py
+++ apps/feedback/admin/question.py
@@ -7,8 +7,3 @@ from apps.feedback.models import Question
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("id", "author_name", "author_email", "question", "sent")
     list_filter = ("sent",)
-
-    def has_module_permission(self, request):
-        if request.user.is_authenticated and (request.user.is_admin or request.user.is_superuser):
-            return super().has_module_permission(request)
-        return False
