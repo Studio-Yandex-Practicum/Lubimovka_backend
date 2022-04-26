21b10779da038ac9ab0cf5c7a32e002555bf3c4a
Omnomus <bka@inbox.ru>
2022-04-26 19:46:08 +0300

    delete print

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -133,7 +133,6 @@ def get_app_list(self, request):
 
 
 def get_domain(request):
-    print(request.is_secure)
     server_protocol = request.META["SERVER_PROTOCOL"].split("/1.1")[0].lower()
     domain = server_protocol + "://" + request.META["HTTP_HOST"]
     return domain
