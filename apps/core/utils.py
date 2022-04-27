e792a9a1c56135dab7e17431d702e32fbb328b2c
Omnomus <bka@inbox.ru>
2022-04-27 13:19:30 +0300

    added decorator_with_args for setting timelimit in cache

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,4 +1,5 @@
 import logging
+import time
 import urllib
 from functools import wraps
 
@@ -95,23 +96,41 @@ def manual_order_model_list(app, admin_site_models_order):
     return app["models"]
 
 
-def cache_user(func):
+def decorator_with_args(decorator_to_enhance):
+    def decorator_maker(*args, **kwargs):
+        def decorator_wrapper(func):
+            return decorator_to_enhance(func, *args, **kwargs)
+
+        return decorator_wrapper
+
+    return decorator_maker
+
+
+@decorator_with_args
+def cache_user(func, *args, **kwargs):
+    """Cache data for timelimit (in seconds)."""
     cache_user_dict = dict()
+    time_user_dict = dict()
+    timelimit = kwargs["timelimit"]
 
     @wraps(func)
     def wrapper(self, request, *args, **kwargs):
         user = request.user.username
+        current_time = round(time.time() / timelimit)
+
         if user in cache_user_dict:
-            return cache_user_dict[user]
+            if user in time_user_dict and current_time == time_user_dict[user]:
+                return cache_user_dict[user]
 
         result = func(self, request, *args, **kwargs)
         cache_user_dict[user] = result
+        time_user_dict[user] = current_time
         return result
 
     return wrapper
 
 
-@cache_user
+@cache_user(timelimit=300)
 def get_app_list(self, request):
     admin_site_apps_order = getattr(settings, "ADMIN_SITE_APPS_ORDER", None)
     admin_site_models_order = getattr(settings, "ADMIN_SITE_MODELS_ORDER", None)
