d494d32526d869795a4f6345b455c74b835c4a49
Nikolay <nikolay@kiryanov.ru>
2022-05-06 01:41:48 +0300

    Smoke tests for info APIs. Settings selector updated

--- apps/info/tests/test_urls.py
+++ apps/info/tests/test_urls.py
@@ -7,7 +7,9 @@ pytestmark = pytest.mark.django_db
 @pytest.mark.parametrize(
     "url",
     (
+        "/api/v1/info/press-releases/years/",
         "/api/v1/info/partners/",
+        "/api/v1/info/settings/",
         "/api/v1/info/festivals/years/",
         "/api/v1/info/about-festival/selectors/",
         "/api/v1/info/about-festival/sponsors/",
@@ -25,3 +27,10 @@ def test_info_url_festival_detail_smoke(client, festival_2020):
     """Smoke test for exact festival."""
     response = client.get("/api/v1/info/festivals/2020/")
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_info_contacts_api_is_broken_have_to_be_fixed(client):
+    """Delete and add the URL to smoke test when it will fixed."""
+    with pytest.raises(AssertionError, match=r"Ключа настроек `email_send_to` не найдено."):
+        response = client.get("/api/v1/info/contacts/")
+        assert response.status_code == 200
