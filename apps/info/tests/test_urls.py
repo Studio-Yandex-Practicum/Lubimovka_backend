39204c9c9a3a4ac5d7a3293fc5e862fd2c156b36
Nikolay <nikolay@kiryanov.ru>
2022-05-07 00:57:08 +0300

    Unnecessary test removed.

--- apps/info/tests/test_urls.py
+++ apps/info/tests/test_urls.py
@@ -27,10 +27,3 @@ def test_info_url_festival_detail_smoke(client, festival_2020):
     """Smoke test for exact festival."""
     response = client.get("/api/v1/info/festivals/2020/")
     assert response.status_code == status.HTTP_200_OK
-
-
-def test_info_contacts_api_is_broken_have_to_be_fixed(client):
-    """Delete and add the URL to smoke test when it will fixed."""
-    with pytest.raises(AssertionError, match=r"Ключа настроек `email_send_to` не найдено."):
-        response = client.get("/api/v1/info/contacts/")
-        assert response.status_code == 200
