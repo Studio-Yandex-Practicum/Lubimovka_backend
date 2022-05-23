f40a719fe4f0cdf1faefa2b010a977833ede5cf9
Omnomus <bka@inbox.ru>
2022-05-23 17:30:33 +0300

    added func to AfishaGetEvent

--- apps/afisha/static/admin/afisha/js/AfishaGetEvent.js
+++ apps/afisha/static/admin/afisha/js/AfishaGetEvent.js
@@ -46,3 +46,37 @@ jQuery(document).ready(function ($) {
         toggleDivDependedOnPartnerType($(this).val(), false);
     });
 });
+'use strict';
+{
+    const $ = django.jQuery;
+
+    $.fn.djangoAdminSelect2 = function(options) {
+        var settings = $.extend({}, options);
+        $.each(this, function(i, element) {
+            var $element = $(element);
+            $.extend(settings, {
+                'ac_field_name': $element.parents().filter('.related-widget-wrapper').find('select').attr('type')
+            });
+            init($element, settings);
+        });
+        return this;
+    };
+
+    var init = function($element, options) {
+        var settings = $.extend({
+            ajax: {
+                data: function(params) {
+                    return {
+                        term: params.term,
+                        page: params.page,
+                        app_label: $element.data('app-label'),
+                        model_name: $element.data('model-name'),
+                        field_name: $element.data('field-name'),
+                        type_name: options.ac_field_name
+                    };
+                }
+            }
+        }, options);
+        $element.select2(settings);
+    };
+}
