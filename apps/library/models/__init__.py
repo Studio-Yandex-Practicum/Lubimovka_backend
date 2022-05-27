574d98ceacfb160c07816d18b982d263a490b2a7
Степан Киселев <Stepakiselev@mail.ru>
2022-05-27 15:25:04 +0300

    unit migrations in afisha

--- apps/library/models/__init__.py
+++ apps/library/models/__init__.py
@@ -1,3 +1,3 @@
-from apps.library.models.author import Achievement, Author, AuthorPlay, OtherLink, SocialNetworkLink
+from apps.library.models.author import Author, AuthorPlay, OtherLink, SocialNetworkLink
 from apps.library.models.play import Play, ProgramType
 from apps.library.models.team_member import TeamMember
