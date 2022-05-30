1b2712af1f7cfa8938f8f2fb93a6d6a28f5aa688
Omnomus <bka@inbox.ru>
2022-05-30 16:27:37 +0300

    added reorder_all command

---
+++ apps/core/management/commands/reorder_all.py
@@ -0,0 +1,21 @@
+import os
+from typing import Any, Optional
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Устанавливает сортировку объектов при создании их с помощью фабрик."
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        models = (
+            "info.FestTeamMember",
+            "info.Partner",
+            "info.Sponsor",
+            "info.Place",
+        )
+        try:
+            for model in models:
+                os.system(f"./manage.py reorder {model}")
+        except CommandError:
+            self.stdout.write(self.style.ERROR("Ошибка выполнения команды."))
