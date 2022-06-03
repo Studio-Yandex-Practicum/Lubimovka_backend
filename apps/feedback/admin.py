3f35dd195245779f37f2272f05ffaf72314a2218
Alexander Rubtsov <equester@mail.ru>
2022-06-03 23:38:18 +0300

    Add custom filter for application date

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,7 +1,37 @@
+import datetime
+
 from django.contrib import admin
 from django.utils import formats
 
 from apps.feedback.models import ParticipationApplicationFestival, Question
+
+
+class LookBackDateListFilter(admin.SimpleListFilter):
+    """Ретроспективный фильтр по датам."""
+
+    title = "Дата создания"
+    parameter_name = "created"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("today", "Сегодня"),
+            ("seven_days", "Последние 7 дней"),
+            ("this_month", "Этот месяц"),
+            ("this_year", "Этот год"),
+        )
+
+    def queryset(self, request, queryset):
+        today = datetime.datetime.now().date()
+        date_ranges = {
+            "today": today,
+            "seven_days": today - datetime.timedelta(days=7),
+            "this_month": today.replace(day=1),
+            "this_year": today.replace(day=1, month=1),
+        }
+        since_date = date_ranges.get(self.value())
+        if not since_date:
+            return queryset
+        return queryset.filter(created__gte=since_date)
 
 
 @admin.register(ParticipationApplicationFestival)
@@ -21,10 +51,9 @@ class ParticipationAdmin(admin.ModelAdmin):
         "saved_to_storage",
         "festival_year",
         "sent_to_email",
-        "created",
+        LookBackDateListFilter,
     )
 
-    readonly_fields = ("created",)
     search_fields = ("title", "first_name", "last_name", "city", "year")
 
     @admin.display(description="Создана")
