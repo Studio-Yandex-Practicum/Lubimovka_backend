8dcff8001de321e71cfd176f9291f0615fe625c6
Степан Киселев <Stepakiselev@mail.ru>
2022-06-09 17:33:17 +0300

    fix upload_to in Play model

--- apps/content_pages/utilities.py
+++ apps/content_pages/utilities.py
@@ -4,3 +4,11 @@ def path_by_app_label_and_class_name(instance, filename):
     class_name_lower = instance.__class__.__name__.lower()
     path = f"images/{app_label}/{class_name_lower}s/{filename}"
     return path
+
+
+def path_by_file_app_label_and_class_name(instance, filename):
+    """Return path in format /`app_label`/`class_name`s/`filename`."""
+    app_label = instance._meta.app_label
+    class_name_lower = instance.__class__.__name__.lower()
+    path = f"file/{app_label}/{class_name_lower}s/{filename}"
+    return path
