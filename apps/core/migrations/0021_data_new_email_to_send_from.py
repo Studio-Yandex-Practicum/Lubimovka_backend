55ca435c6365a44264d72d89b5b05345228d2073
Omnomus <bka@inbox.ru>
2022-06-09 14:12:24 +0300

    change descroption for email

--- apps/core/migrations/0021_data_new_email_to_send_from.py
+++ apps/core/migrations/0021_data_new_email_to_send_from.py
@@ -5,10 +5,25 @@ def change_email_settings(apps, schema_editor):
 
     Setting = apps.get_model("core", "Setting")
 
-    values_for_update_email_send_from = {"text": "admin@lubimovka.ru"}
+    values_for_update_email_send_from = {
+        "text": "admin@lubimovka.ru",
+        "description": "Почта для отправки писем с сайта"
+    }
     Setting.objects.update_or_create(
         settings_key="email_send_from",
         defaults=values_for_update_email_send_from,
+    )
+
+    values_for_update_email_to_send_questions = {"description": "Почта для приёма вопросов"}
+    Setting.objects.update_or_create(
+        settings_key="email_to_send_questions",
+        defaults=values_for_update_email_to_send_questions,
+    )
+
+    values_for_update_email_to_send_participations = {"description": "Почта для приёма заявок на участие"}
+    Setting.objects.update_or_create(
+        settings_key="email_to_send_participations",
+        defaults=values_for_update_email_to_send_participations,
     )
 
 class Migration(migrations.Migration):
