ebda4b1c1735fd0105784388e1e2ca1c31bfad06
Степан Киселев <Stepakiselev@mail.ru>
2022-06-09 18:36:30 +0300

    fix path_by_media_app_label_and_class_name

--- apps/content_pages/utilities.py
+++ apps/content_pages/utilities.py
@@ -7,8 +7,7 @@ def path_by_app_label_and_class_name(instance, filename):
 
 
 def path_by_media_app_label_and_class_name(instance, filename):
-    """Return path in format /`app_label`/`class_name`s/`filename`."""
-    app_label = instance._meta.app_label
+    """Return path in format media/`class_name`s/`filename`."""
     class_name_lower = instance.__class__.__name__.lower()
-    path = f"{app_label}/{class_name_lower}s/{filename}"
+    path = f"media/{class_name_lower}s/{filename}"
     return path
