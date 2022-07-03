62c53ca9f4d99f5e30a8235c1579fcc9bf5822f3
Степан Киселев <Stepakiselev@mail.ru>
2022-07-03 17:45:51 +0300

    change creator_name to creator in admin models

--- apps/articles/admin/blog_items.py
+++ apps/articles/admin/blog_items.py
@@ -25,12 +25,12 @@ class BlogItemAdmin(StatusButtonMixin, PreviewButtonMixin, BaseContentPageAdmin)
         "pub_date",
         "image_preview_list_page",
         "status",
-        "creator_name",
+        "creator",
     )
     readonly_fields = (
         "status",
         "image_preview_change_page",
-        "creator_name",
+        "creator",
     )
     inlines = (
         BlogPersonInline,
@@ -47,7 +47,7 @@ class BlogItemAdmin(StatusButtonMixin, PreviewButtonMixin, BaseContentPageAdmin)
                     "pub_date",
                     "description",
                     ("image_preview_change_page", "image"),
-                    "creator_name",
+                    "creator",
                 )
             },
         ),
--- apps/articles/admin/news_items.py
+++ apps/articles/admin/news_items.py
@@ -19,12 +19,12 @@ class NewsItemAdmin(StatusButtonMixin, PreviewButtonMixin, BaseContentPageAdmin)
         "pub_date",
         "image_preview_list_page",
         "status",
-        "creator_name",
+        "creator",
     )
     readonly_fields = (
         "status",
         "image_preview_change_page",
-        "creator_name",
+        "creator",
     )
     inlines = (NewsItemContentInline,)
     fieldsets = (
@@ -37,7 +37,7 @@ class NewsItemAdmin(StatusButtonMixin, PreviewButtonMixin, BaseContentPageAdmin)
                     "pub_date",
                     "description",
                     ("image_preview_change_page", "image"),
-                    "creator_name",
+                    "creator",
                 )
             },
         ),
