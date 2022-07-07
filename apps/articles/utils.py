bd6b24e25f15664f9d03cde7325aca7263b98a62
Степан Киселев <Stepakiselev@mail.ru>
2022-07-07 20:27:22 +0200

    change creator_name to creator

--- apps/articles/utils.py
+++ apps/articles/utils.py
@@ -6,6 +6,6 @@ def check_journalist_perms(request, obj):
     """Journalist can edit only own items."""
     creator = LogEntry.objects.filter(object_id=obj.id, content_type=get_content_type_for_model(obj)).first()
     visitor = request.user
-    if creator and visitor.is_staff and visitor.username != creator.user.username:
+    if creator and visitor.is_staff and visitor != creator.user:
         return 0
     return 1
