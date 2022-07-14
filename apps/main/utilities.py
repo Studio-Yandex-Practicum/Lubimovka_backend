640f7f84f31757b94a2ecbc249ec917afe41fa9b
Omnomus <bka@inbox.ru>
2022-07-14 18:35:34 +0300

    added random sorting to short_list items

--- apps/main/utilities.py
+++ apps/main/utilities.py
@@ -99,7 +99,7 @@ class MainObject:
                 program=program,
                 festival=festival,
                 published=True,
-            )
+            ).order_by("?")
             title = Setting.get_setting("main_short_list_title")
             self.short_list = {
                 "title": title,
