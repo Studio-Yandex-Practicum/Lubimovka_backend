496a6050b81312c79e79b13f89deba384ba943cb
Alexander Rubtsov <equester@mail.ru>
2022-08-03 19:12:25 +0300

    Add seacrh by name to role admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -23,7 +23,7 @@ class RoleAdmin(admin.ModelAdmin):
         "name",
         "slug",
     )
-    search_fields = ("role",)
+    search_fields = ("role", "name")
     prepopulated_fields = {"slug": ("name",)}
     list_filter = ("types",)
     formfield_overrides = {
