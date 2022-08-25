55cab7b7d235ea5f725fa99c04fb285a18890f78
Степан Киселев <Stepakiselev@mail.ru>
2022-08-25 21:12:19 +0600

    add filter_by_name_predicate to AuthorFilter

--- apps/library/filters/authors.py
+++ apps/library/filters/authors.py
@@ -1,8 +1,12 @@
 import django_filters
+from django.db.models import Q
+
+
+def filter_by_name_predicate(queryset, name, value):
+    return queryset.filter(Q(person__first_name__istartswith=value) | Q(person__last_name__istartswith=value))
 
 
 class AuthorFilter(django_filters.FilterSet):
-    letter = django_filters.CharFilter(field_name="person__last_name", lookup_expr="startswith")
+    """Фильтрует по начальным буквам в фамелии и имени."""
 
-    class Meta:
-        fields = ("letter",)
+    letter = django_filters.CharFilter(method=filter_by_name_predicate)
