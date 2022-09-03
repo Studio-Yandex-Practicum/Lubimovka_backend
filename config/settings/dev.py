b8c3b5eded11c17a4a6c29a6fbddf2691acbee61
Vlad Nikitin <nikitinv91@gmail.com>
2022-09-03 16:53:05 -0400

    pep8 fix

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS += ["stage.dev.lubimovka.ru"]
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"] # noqa: F405
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
--- config/settings/local.py
+++ config/settings/local.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS += ["127.0.0.1"]
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"] # noqa: F405
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
