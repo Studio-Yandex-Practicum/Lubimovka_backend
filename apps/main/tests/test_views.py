fa71aacd90297e26b0e1895afbfcdbc27a458832
Vlad Nikitin <nikitinv91@gmail.com>
2022-09-07 09:59:49 -0400

    adjusted expected field names

--- apps/main/tests/test_views.py
+++ apps/main/tests/test_views.py
@@ -203,7 +203,7 @@ class TestMainAPIViews:
         places,
     ):
         """Checks data["afisha"]["items"] in response."""
-        fields = ["id", "type", "event_body", "date_time", "url", "button"]
+        fields = ["id", "type", "event_body", "date_time", "action_url", "action_text"]
         response = client.get(MAIN_URL)
         for field in fields:
             assert (
