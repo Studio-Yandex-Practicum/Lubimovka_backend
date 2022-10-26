e9c7ac736f377081f6d835a0de5f9a92da45ef8e
Alexander Rubtsov <equester@mail.ru>
2022-10-26 19:39:34 +0300

    Change filer routing

--- config/urls.py
+++ config/urls.py
@@ -75,7 +75,7 @@ urlpatterns += [
         view=include(api_schema_patterns),
     ),
     re_path(
-        route=r"^media/",
+        route=r"^files/",
         view=include("filer.urls"),
     ),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
