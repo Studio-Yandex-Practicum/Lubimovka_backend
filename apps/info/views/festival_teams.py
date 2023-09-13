e13b502924372d8c5c06c86bfa51000ec928ab0c
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2023-09-13 13:31:25 +0300

    Apply show settings to API output

--- apps/info/views/festival_teams.py
+++ apps/info/views/festival_teams.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 
+from apps.core.models import Setting
 from apps.info.models import FestivalTeamMember
 from apps.info.serializers import FestivalTeamsSerializer
 
@@ -9,3 +10,10 @@ class FestivalTeamsAPIView(ListAPIView):
     serializer_class = FestivalTeamsSerializer
     filterset_fields = ("team",)
     pagination_class = None
+
+    def get_queryset(self):
+        show = Setting.get_setting("show_team")
+        qs = super().get_queryset()
+        if not show:
+            qs = qs.none()
+        return qs
