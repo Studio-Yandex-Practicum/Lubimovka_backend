1163d6dc33a0e493232e6cfe8b930f0b01abc9b1
ilya <iborovkov97@yandex.ru>
2023-11-03 20:14:46 +0300

    feature question_created_date closed

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.template.defaultfilters import truncatechars
+from django.utils import formats
 
 from apps.feedback.filters import LookBackDateListFilter
 from apps.feedback.models import ParticipationApplicationFestival, Question
@@ -34,9 +35,17 @@ class ParticipationAdmin(admin.ModelAdmin):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("author_name", "author_email", "short_question_text", "sent_to_email")
-    list_filter = ("sent_to_email",)
+    list_display = ("author_name", "author_email", "short_question_text", "sent_to_email", "created_display")
+    list_filter = ("sent_to_email", LookBackDateListFilter)
     search_fields = ("author_name", "author_email", "question")
+    readonly_fields = ("question", "author_name", "author_email", "sent_to_email", "created_display")
+
+    @admin.display(
+        description="Дата создания",
+    )
+    def created_display(self, obj):
+        """Возвращает дату создания объекта."""
+        return formats.date_format(obj.created, format="DATETIME_FORMAT", use_l10n=True)
 
     @admin.display(
         description="Текст вопроса",
@@ -45,5 +54,5 @@ class QuestionAdmin(admin.ModelAdmin):
         """Возвращает текст вопроса, обрезанный до 70 знаков."""
         return truncatechars(obj.question, 70)
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, _):
         return False
