a00cb5d1cdd741ddb2209d450f42971df3286753
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2023-11-27 17:55:37 +0300

    Fix type annotation

--- apps/postfix/validators.py
+++ apps/postfix/validators.py
@@ -6,7 +6,7 @@ MESSAGE = "{message} Введен адрес {email}"
 
 
 class VirtualNameValidator(EmailValidator):
-    def __call__(self, value: str | None) -> None:
+    def __call__(self, value: str) -> None:
         full_email = FULL_EMAIL.format(name=value, domain=settings.POSTFIX_MAIL_DOMAIN)
         self.message = MESSAGE.format(message=EmailValidator.message, email=full_email)
         return super().__call__(full_email)
