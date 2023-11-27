fdb8f2ae91bcab7b2e39ee55a52743ceb8970034
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2023-11-27 17:10:22 +0300

    Change message formatting

--- apps/postfix/validators.py
+++ apps/postfix/validators.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.core.validators import EmailValidator
 
 FULL_EMAIL = "{name}@{domain}"
+MESSAGE = "{message} Введен адрес {email}"
 
 
 class VirtualNameValidator(EmailValidator):
     def __call__(self, value: str | None) -> None:
         full_email = FULL_EMAIL.format(name=value, domain=settings.POSTFIX_MAIL_DOMAIN)
-        self.message = EmailValidator.message + f" Введен адрес {full_email}."
+        self.message = MESSAGE.format(message=EmailValidator.message, email=full_email)
         return super().__call__(full_email)
