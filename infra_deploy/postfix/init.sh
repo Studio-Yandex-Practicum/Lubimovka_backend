5ed0e0f02d57fa396f254ea29a446afc5af25b95
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-03-15 15:10:35 +0300

    Fix regexp

--- infra_deploy/postfix/init.sh
+++ infra_deploy/postfix/init.sh
@@ -1,6 +1,6 @@
 sed -i "s/^dbname = .*$/dbname = $POSTGRES_DB/g" /etc/postfix/pgsql-virtual.cf
 sed -i "s/^password = .*$/password = $POSTFIX_DB_PASSWORD/g" /etc/postfix/pgsql-virtual.cf
-sed -i "s/^MY_DOMAIN=.*/MY_DOMAIN=$POSTFIX_MAIL_DOMAIN/g" /home/filter/filter.sh
+sed -i "s/^MY_DOMAIN=.*$/MY_DOMAIN=$POSTFIX_MAIL_DOMAIN/g" /home/filter/filter.sh
 
 touch /etc/postfix/blacklist
 touch /etc/postfix/whitelist
