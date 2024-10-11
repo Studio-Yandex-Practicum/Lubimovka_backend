4919f59e5e4a46349362b11e7bc9248bc51537be
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-10-11 16:01:49 +0300

    Refactor hidden path generator

--- apps/library/services.py
+++ apps/library/services.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from django.conf import settings
 
 
-def _hidden_path(play) -> Path:
+def _hidden_path(media_path: Path) -> Path:
     """Вычисление пути к скрытому хранилищу."""
-    return settings.HIDDEN_MEDIA_ROOT / Path(play.url_download.path).relative_to(settings.MEDIA_ROOT)
+    return settings.HIDDEN_MEDIA_ROOT / media_path.relative_to(settings.MEDIA_ROOT)
 
 
 def restore_play_file(play):
@@ -13,7 +13,7 @@ def restore_play_file(play):
     if not play or not play.url_download:
         return
     regular_play_file = Path(play.url_download.path)
-    hidden_play_file = _hidden_path(play)
+    hidden_play_file = _hidden_path(regular_play_file)
     if hidden_play_file.is_file():
         hidden_play_file.replace(regular_play_file)
 
@@ -23,6 +23,6 @@ def hide_play_file(play):
     if not play or not play.url_download:
         return
     regular_play_file = Path(play.url_download.path)
-    hidden_play_file = _hidden_path(play)
+    hidden_play_file = _hidden_path(regular_play_file)
     if regular_play_file.is_file():
         regular_play_file.replace(hidden_play_file)
